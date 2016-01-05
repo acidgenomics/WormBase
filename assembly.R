83674286bac96cbda72a50655c7434cb65c95a79
Michael Steinbaugh <mike@steinbaugh.com>
2016-01-05 18:01:15 -0500

    code cleanup
    preparation for initial public release

--- assembly.R
+++ assembly.R
@@ -1,20 +1,41 @@
+# C. elegans metadata assembly
+# Michael J. Steinbaugh, PhD
+# email: mike@steinbaugh.com
+# twitter: @mjsteinbaugh
+
+# Please don't copy or fork any code without contacting me first!
+
 rm(list = ls(all.names = T))
 if (!file.exists("save")) { dir.create("save") }
+
+# install required packages
+source("http://bioconductor.org/biocLite.R")
+biocLite()
+biocLite("BiocUpgrade")
+biocLite(c("biomaRt",
+					 "plyr",
+					 "RCurl",
+					 "readr",
+					 "stringr"))
+install.packages("openxlsx", dependencies = T)
 
 # get files from wormbase and panther
 source("downloads.R")
 
 source("GeneID.R")
-source("biomart.R")
 source("descriptions.R")
+source("biomart.R")
+source("rnai.phenotypes.R")
+source("blastp.R")
 source("orthologs.R")
 source("panther.R")
-source("rnai.phenotypes.R")
 
+rm(list = ls(all.names = T))
 # load the saved values
 load("save/GeneID.rda")
-load("save/biomart.rda")
 load("save/descriptions.rda")
+load("save/biomart.rda")
+load("save/blastp.rda")
 load("save/orthologs.rda")
 load("save/panther.rda")
 load("save/rnai.phenotypes.rda")
@@ -22,65 +43,26 @@ load("save/rnai.phenotypes.rda")
 # compile
 df <- data.frame()
 df <- cbind(GeneID,
-						biomart,
 						descriptions,
+						biomart,
 						rnai.phenotypes,
+						blastp,
 						orthologs,
 						panther)
 colnames(df)
-df.all <- df
-
-df <- df[,c("GeneID",
-						"public.name",
-						"ORF",
-						"wormbase.status",
-						"gene.biotype",
-						"gene.class.description",
-						"concise.description",
-						"provisional.description",
-						"automated.description",
-						"ensembl.description",
-						"hsapiens.homolog.ensembl.gene",
-						"hsapiens.homolog.wormbase.id",
-						"hsapiens.homolog.wormbase.name",
-						"mmusculus.homolog.ensembl.gene",
-						"mmusculus.homolog.wormbase.id",
-						"mmusculus.homolog.wormbase.name",
-						"rnai.phenotypes",
-						"chromosome.name",
-						"start.position",
-						"end.position",
-						"strand",
-						"entrezgene",
-						"uniprot.sptrembl", # from biomart
-						"uniprot.swissprot", # from biomart
-						"uniprot.kb", # from panther
-						"ensembl.go.id",
-						"ensembl.go.names",
-						"interpro",
-						"interpro.short.description",
-						"interpro.description",
-						"panther.sf.id",
-						"panther.family.name",
-						"panther.subfamily.name",
-						"panther.go.bp",
-						"panther.go.mf",
-						"panther.go.cc",
-						"panther.pc",
-						"panther.pathway")]
 metadata <- df
-write.csv(metadata,"metadata.csv",row.names = F)
+write.csv(metadata, "metadata.csv", row.names = F)
 
 # simple
 metadata.simple <- df[,c("GeneID","ORF","public.name")]
-write.csv(metadata.simple,"metadata.simple.csv",row.names = F)
+write.csv(metadata.simple, "metadata.simple.csv", row.names = F)
 
 # rownames by ORF instead of GeneID (Wormbase ID)
 metadata.ORF <- df[,c("ORF","GeneID","public.name")]
 metadata.ORF <- metadata.ORF[!duplicated(metadata.ORF$ORF),]
-metadata.ORF <- subset(metadata.ORF,!is.na(metadata.ORF$ORF))
+metadata.ORF <- subset(metadata.ORF, !is.na(metadata.ORF$ORF))
 rownames(metadata.ORF) <- metadata.ORF$ORF
-write.csv(metadata.ORF,"metadata.ORF.csv",row.names = F)
+write.csv(metadata.ORF, "metadata.ORF.csv", row.names = F)
 
 save(metadata,metadata.ORF,metadata.simple, file = "save/metadata.rda")
 save.image("save/assembly.RData")
@@ -89,3 +71,7 @@ save.image("save/assembly.RData")
 system("gzip --force metadata.csv")
 system("gzip --force metadata.ORF.csv")
 system("gzip --force metadata.simple.csv")
+
+# update ORFome RNAi metadata for screening info
+# be sure to run last -- CPU intensive and requires compiled metadata.rda
+source("orfeome.R")
---
+++ orfeome.R
@@ -0,0 +1,48 @@
+rm(list = ls(all.names = T))
+pkg <- c("openxlsx")
+lapply(pkg,require,character.only = T)
+
+load("save/metadata.rda")
+
+input <- read.xlsx("downloads/cernai-feeding-library.xlsx", sheet = 2)
+save(input, file = "save/cernai.feeding.library.rda")
+df <- input
+df <- subset(df, !is.na(`ORF.ID.(WS112)`))
+df <- subset(df, `ORF.ID.(WS112)` != "no match in WS112")
+cols <- c("Plate","Row","Col")
+df$id
+
+vec <- do.call(paste, c(df[cols], sep = "-"))
+vec <- gsub("^([0-9]{5})-(.*)$", "\\1@\\2", vec, perl = T, ignore.case = F)
+vec <- gsub("^(.*)-([0-9]{1})$", "\\10\\2", vec, perl = T, ignore.case = F) # pad zeros for sorting later
+vec <- gsub("^(.*)-([0-9]{2})$", "\\1\\2", vec, perl = T, ignore.case = F)
+head(vec)
+rownames(df) <- vec
+# remove columns from original df
+#!!! for (co in cols) data[co] <- NULL
+
+# get info for duplicate clones
+dupes <- df[duplicated(df$`ORF.ID.(WS112)`),]
+
+# ORF cleanup for matching
+vec <- as.vector(df$`ORF.ID.(WS112)`)
+# strip any isoform annotations off the end for proper matching!
+vec <- gsub("^(.*)[a-z]{1}$", "\\1", vec, perl = T, ignore.case = F)
+head(vec)
+orf <- vec
+
+# since there are duplicate ORFs per well, we must set a loop and pull from metadata.ORF
+# alternate approach to use lapply somehow would be faster?
+info <- data.frame(matrix(nrow = 0, ncol = 3))
+colnames(info) <- colnames(metadata.ORF)
+for (i in 1:length(orf)) {
+	info <- rbind(info, metadata.ORF[orf[i],])
+}
+rownames(info) <- rownames(df)
+info <- info[,c(2,1,3)] # flip ORF and GeneID order
+ORFeome.RNAi.pheno <- df$WormB.RNAi.Pheno
+info <- cbind(info,ORFeome.RNAi.pheno)
+orfeome <- info
+save(orfeome, file = "save/orfeome.rda")
+
+save.image("save/orfeome.RData")
