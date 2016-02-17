e2723090ffa4db58c4781caf85429db31e3e6d2f
Michael Steinbaugh <mike@steinbaugh.com>
2016-02-17 15:53:52 -0500

    Switch to underscore naming

--- GeneID.R
+++ GeneID.R
@@ -2,8 +2,8 @@ df <- read.csv("sources/geneIDs.txt.gz", header = F, na.strings = "")
 df <- df[, c(2:5)] # discard uneeded columns
 colnames(df) <- c("GeneID", "public.name", "ORF", "wormbase.status")
 rownames(df) <- df$GeneID
-GeneID.vec <- rownames(df)
+GeneID_vec <- rownames(df)
 GeneID <- df
 rm(df)
 
-save(GeneID, GeneID.vec, file = "rda/GeneID.rda")
+save(GeneID, GeneID_vec, file = "rda/GeneID.rda")
