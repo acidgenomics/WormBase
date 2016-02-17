b581a5532044695edc8e2363dc1526c012aecadd
Michael Steinbaugh <mike@steinbaugh.com>
2016-02-17 16:31:49 -0500

    code cleanup and rebuild

--- orfeome.R
+++ orfeome.R
@@ -1,11 +1,10 @@
 pkg <- c("openxlsx")
-lapply(pkg, require, character.only = T)
-
+lapply(pkg, require, character.only = TRUE)
 load("rda/metadata.rda")
-rm(metadata, metadata.simple)
 
 input <- read.xlsx("sources/orfeome.xlsx", sheet = 2)
-df <- input[, c("ORF.ID.(WS112)",
+
+x <- input[, c("ORF.ID.(WS112)",
                 "Plate",
                 "Row",
                 "Col",
@@ -13,53 +12,66 @@ df <- input[, c("ORF.ID.(WS112)",
                 "Nonv",
                 "Vpep",
                 "Simmer")]
-names(df)[names(df) == "ORF.ID.(WS112)"] <- "ORF"
+names(x)[names(x) == "ORF.ID.(WS112)"] <- "ORF"
 
-# Censors or wells without match
-censored <- subset(df, is.na(RNAi.well))
-no.match <- subset(df, ORF == "no match in WS112")
-
-# Subset the censors
-df <- subset(df, !is.na(RNAi.well))
-df <- subset(df, ORF != "no match in WS112")
-input.clean <- df
+# Subset the bad wells
+x <- subset(x, !is.na(RNAi.well))
+x <- subset(x, ORF != "no match in WS112")
+input_subset <- x
+rm(x)
 
 # Set plate IDs as rownames
+x <- input_subset
 col <- c("Plate", "Row", "Col")
-ORFeomeID <- do.call(paste, c(df[col], sep = "-"))
+ORFeomeID <- do.call(paste, c(x[col], sep = "-"))
 rm(col)
 ORFeomeID[1]
-ORFeomeID <- gsub("^(.*)-([0-9]{1})$", "\\1-0\\2", ORFeomeID, perl = T, ignore.case = F) # pad zeros
+ORFeomeID <- gsub("^(.*)-([0-9]{1})$", "\\1-0\\2", ORFeomeID,
+                  perl = TRUE, ignore.case = FALSE) # pad zeros
 ORFeomeID[1]
-ORFeomeID <- gsub("^([0-9]{5})-([A-Z]{1})-([0-9]{2})$", "\\1@\\2\\3", ORFeomeID, perl = T, ignore.case = F)
+ORFeomeID <- gsub("^([0-9]{5})-([A-Z]{1})-([0-9]{2})$", "\\1@\\2\\3", ORFeomeID,
+                  perl = TRUE, ignore.case = FALSE)
 ORFeomeID[1]
-df <- cbind(ORFeomeID, df)
-rownames(df) <- df$ORFeomeID
-orfeome.valid <- df
+x <- cbind(ORFeomeID, x)
+rownames(x) <- x$ORFeomeID
+orfeome_valid <- x
+rm(x, ORFeomeID)
 
-# Since there are duplicate ORFs per well, we must loop from metadata.ORF
-orf <- as.vector(df$ORF)
+# Since there are duplicate ORFs per well, we must loop from metadata_ORF
+# This will take a few minutes to run on a laptop
+x <- orfeome_valid
+orf <- as.vector(x$ORF)
 list <- list()
 list <- lapply(seq(along = orf), function(i) {
-  metadata.ORF[orf[i], c(2, 3)]
+  metadata_ORF[orf[i], ]
 })
-# This step is cpu expensive, any way to speed up?
-df <- data.frame(do.call("rbind", list))
-rownames(df) <- NULL
-orf.to.GeneID <- df
+x <- data.frame(do.call("rbind", list))
+rownames(x) <- NULL
+head(x)
+ORF_to_GeneID <- x
+rm(x, list, orf)
 
-# bind the matches back to the valid orfeome df
-df <- cbind(orf.to.GeneID, orfeome.valid)
-rownames(df) <- as.vector(df$ORFeomeID)
-df$ORFeomeID <- NULL
-colnames(df)
-df <- df[, c("RNAi.well", "ORF", "GeneID", "public.name",
-             "Nonv", "Vpep", "Simmer")]
-orfeome <- df
+# Bind the matches back to the valid orfeome data frame
+x <- data.frame()
+x <- cbind(ORF_to_GeneID, orfeome_valid)
+rownames(x) <- as.vector(x$ORFeomeID)
+# Remove the first ORF instance (from ORF_to_GeneID)
+x$ORF <- NULL
+x$ORFeomeID <- NULL
+colnames(x)
 
-orfeome.unmatched <- orfeome[is.na(orfeome$GeneID), ]
-orfeome.simple <- orfeome[, c(2:4)]
-orfeome.unique <- unique(as.vector(orfeome$ORF))
+# Reorder the columns for better presentation
+# This will remove Plate, Row, Col
+x <- x[, c("RNAi.well",
+           "ORF", "GeneID", "public.name",
+           "Nonv", "Vpep", "Simmer")]
+orfeome <- x
+orfeome_simple <- x[, c("ORF", "GeneID", "public.name")]
+rm(x)
 
-save(orfeome, orfeome.simple, file = "rda/orfeome.rda")
+# Additional information useful for library troubleshooting
+orfeome_unmatched <- orfeome[is.na(orfeome$GeneID), ]
+orfeome_unique <- unique(as.vector(orfeome$ORF))
+
+save(orfeome, orfeome_simple, file = "rda/orfeome.rda")
 write.csv(orfeome, "csv/orfeome.csv")
