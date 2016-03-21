1486d6705a63183b4f45af9e4717b39ef04150d9
Michael Steinbaugh <mike@steinbaugh.com>
2016-03-21 12:00:53 -0400

    Update to include other ID info

--- orfeome.R
+++ orfeome.R
@@ -1,26 +1,28 @@
-pkg <- c("openxlsx")
+pkg <- c("readxl")
 lapply(pkg, require, character.only = TRUE)
 load("rda/metadata.rda")
 
+# Import the Excel file ========================================================
 input <- read_excel("sources/orfeome.xlsx", sheet = 2)
+colnames(input) <- gsub(" ", ".", colnames(input))
 
+# Set up the data frame converted from Excel ===================================
+x <- data.frame()
 x <- input[, c("ORF.ID.(WS112)",
-                "Plate",
-                "Row",
-                "Col",
-                "RNAi.well",
-                "Nonv",
-                "Vpep",
-                "Simmer")]
+               "Plate",
+               "Row",
+               "Col",
+               "RNAi.well",
+               "Nonv",
+               "Vpep",
+               "Simmer")]
 names(x)[names(x) == "ORF.ID.(WS112)"] <- "ORF"
-
-# Subset the bad wells
+# Subset the bad wells =========================================================
 x <- subset(x, !is.na(RNAi.well))
 x <- subset(x, ORF != "no match in WS112")
 input_subset <- x
 rm(x)
-
-# Set plate IDs as rownames
+# Set plate IDs as rownames ====================================================
 x <- input_subset
 col <- c("Plate", "Row", "Col")
 ORFeomeID <- do.call(paste, c(x[col], sep = "-"))
@@ -36,42 +38,48 @@ x <- cbind(ORFeomeID, x)
 rownames(x) <- x$ORFeomeID
 orfeome_valid <- x
 rm(x, ORFeomeID)
-
+# Get current metadata =========================================================
 # Since there are duplicate ORFs per well, we must loop from metadata_ORF
-# This will take a few minutes to run on a laptop
 x <- orfeome_valid
 orf <- as.vector(x$ORF)
 list <- list()
 list <- lapply(seq(along = orf), function(i) {
   metadata_ORF[orf[i], ]
 })
+# Converting to a data frame here will take a while
 x <- data.frame(do.call("rbind", list))
-rownames(x) <- NULL
-head(x)
 ORF_to_GeneID <- x
 rm(x, list, orf)
-
-# Bind the matches back to the valid orfeome data frame
+# Bind the matches back to the valid orfeome data frame ------------------------
 x <- data.frame()
 x <- cbind(ORF_to_GeneID, orfeome_valid)
 rownames(x) <- as.vector(x$ORFeomeID)
-# Remove the first ORF instance (from ORF_to_GeneID)
+# Remove duplicate columns -----------------------------------------------------
+# This will only remove the first instance, here from ORF_to_GeneID
 x$ORF <- NULL
 x$ORFeomeID <- NULL
+# Remove unnecessary plate information
+x$Plate <- NULL
+x$Row <- NULL
+x$Col <- NULL
 colnames(x)
-
-# Reorder the columns for better presentation
 # This will remove Plate, Row, Col
 x <- x[, c("RNAi.well",
-           "ORF", "GeneID", "public.name",
-           "Nonv", "Vpep", "Simmer")]
+           "ORF",
+           "GeneID",
+           "public.name",
+           "gene.other.ids",
+           "Nonv",
+           "Vpep",
+           "Simmer")]
 orfeome <- x
-orfeome_simple <- x[, c("ORF", "GeneID", "public.name")]
+orfeome_simple <- x[, c("ORF", "GeneID", "public.name", "gene.other.ids")]
 rm(x)
 
-# Additional information useful for library troubleshooting
+# Additional information for library troubleshooting ===========================
 orfeome_unmatched <- orfeome[is.na(orfeome$GeneID), ]
 orfeome_unique <- unique(as.vector(orfeome$ORF))
 
+# Save =========================================================================
 save(orfeome, orfeome_simple, file = "rda/orfeome.rda")
 write.csv(orfeome, "csv/orfeome.csv")
