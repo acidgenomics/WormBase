af7f3aeb32b55a0e044fb355d6c5dfe044bffdf4
Michael Steinbaugh <mike@steinbaugh.com>
2016-03-21 12:01:47 -0400

    Add other ID info from Wormbase

--- GeneID.R
+++ GeneID.R
@@ -1,3 +1,6 @@
+pkg <- c("readr", "stringr")
+lapply(pkg, require, character.only = T)
+# GeneID =======================================================================
 df <- read.csv("sources/geneIDs.txt.gz", header = F, na.strings = "")
 df <- df[, c(2:5)] # discard uneeded columns
 colnames(df) <- c("GeneID", "public.name", "ORF", "wormbase.status")
@@ -5,5 +8,22 @@ rownames(df) <- df$GeneID
 GeneID_vec <- rownames(df)
 GeneID <- df
 rm(df)
+
+# Other IDs ====================================================================
+file <- read_file("sources/geneOtherIDs.txt.gz")
+# Take out dead or live status, we have this from geneIDs.txt
+file <- gsub("\t(Dead|Live)", "", file, perl = TRUE)
+# Take the tabs out for gene list
+file <- gsub("\t", ", ", file)
+# Add tab back in to separate GeneID for row names
+file <- gsub("WBGene([0-9]+), ", "WBGene\\1\t", file, perl = TRUE)
+# Parsing failures here means there are no other IDs for that row
+df <- read_tsv(file, col_names = FALSE)
+rownames(df) <- df[, 1]
+colnames(df) <- c("GeneID", "gene.other.ids")
+df <- df[GeneID_vec, ]
+df$GeneID <- NULL
+GeneID <- cbind(GeneID, df)
+rm(df, file)
 
 save(GeneID, GeneID_vec, file = "rda/GeneID.rda")
