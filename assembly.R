bf365b73f7a20b5b5eed6d84b445e6b6d8ad902d
Michael Steinbaugh <mike@steinbaugh.com>
2016-03-22 00:26:48 -0400

    rebuild

--- assembly.R
+++ assembly.R
@@ -1,53 +1,69 @@
-#! Ahringer and cherrypick update needed
-
-source("http://bioconductor.org/biocLite.R")
-biocLite()
-biocLite(c("biomaRt", "plyr", "RCurl", "readr", "stringr"))
-
-# https://github.com/hadley/readxl
-# readxl is needed to open RNAi library spreadsheets
-install.packages("readxl")
-
-# Start assembly!
 rm(list = ls(all.names = TRUE))
-if (!file.exists("csv")) { dir.create("csv") }
-if (!file.exists("rda")) { dir.create("rda") }
+# Install packages if necessary ================================================
+# Bioconductor packages --------------------------------------------------------
+pkg <- c("biomaRt", "plyr", "RCurl", "readr", "stringr")
+install_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+if (length(install_pkg) > 0) {
+  source("http://bioconductor.org/biocLite.R")
+  biocLite()
+  biocLite(install_pkg)
+}
+lapply(pkg, require, character.only = TRUE)
+# CRAN packages ----------------------------------------------------------------
+pkg <- c("readxl")
+install_pkg <- pkg[!(pkg %in% installed.packages()[, "Package"])]
+if (length(install_pkg) > 0) {
+  install.packages(install_pkg)
+}
+lapply(pkg, require, character.only = TRUE)
+rm(install_pkg, pkg)
+(.packages())
 
-# Download current files from WormBase and PANTHER -----------------------------
+# Set up the output folders ====================================================
+if (!file.exists("csv")) {
+  unlink("csv", recursive = TRUE)
+}
+if (!file.exists("rda")) {
+  unlink("rda", recursive = TRUE)
+}
+dir.create("csv")
+dir.create("rda")
+
+# Download current files from WormBase and PANTHER =============================
 source("sources.R", verbose = TRUE)
 
-# Build the datasets -----------------------------------------------------------
+# Source the .R code files =====================================================
 datasets <- c("GeneID", "description", "rnai_phenotypes", "blastp",
               "orthologs", "biomart", "panther")
-
-# Source the .R code files -----------------------------------------------------
 lapply(seq(along = datasets), function(i) {
   file <- paste(c(datasets[i], ".R"), collapse = "")
   source(file, verbose = TRUE)
 })
-
-# Load the saved .rda files for compile ----------------------------------------
+# Now load the saved .rda files for compile ------------------------------------
 lapply(seq(along = datasets), function(i) {
   file <- paste(c("rda/",datasets[i], ".rda"), collapse = "")
   load(file, .GlobalEnv)
 })
 
-# Compile the master metadata data.frame ---------------------------------------
+# Compile the master metadata data.frame =======================================
 metadata <- data.frame()
 metadata <- do.call(cbind, mget(datasets))
 colnames(metadata) <- gsub("GeneID.", "", colnames(metadata))
 write.csv(metadata, "csv/metadata.csv", row.names = FALSE)
 
-# Save a list of available columns ---------------------------------------------
+# Save a list of available columns =============================================
 colnames <- colnames(metadata)
 colnames
 write(colnames, "colnames.txt", sep = "\n")
 
-# Simple version ---------------------------------------------------------------
-metadata_simple <- metadata[, c("GeneID", "ORF", "public.name", "gene.other.ids")]
+# Simple version ===============================================================
+metadata_simple <- metadata[, c("GeneID",
+                                "ORF",
+                                "public.name",
+                                "gene.other.ids")]
 write.csv(metadata_simple, "csv/metadata_simple.csv", row.names = FALSE)
 
-# Rownames by ORF instead of GeneID (Wormbase ID) ------------------------------
+# Rownames by ORF instead of GeneID (Wormbase ID) ==============================
 metadata_ORF <- metadata_simple
 metadata_ORF <- subset(metadata_ORF, !is.na(metadata_ORF$ORF))
 metadata_ORF <- metadata_ORF[!duplicated(metadata_ORF$ORF), ]
@@ -56,19 +72,18 @@ write.csv(metadata_ORF, "csv/metadata_ORF.csv", row.names = FALSE)
 
 save(metadata, metadata_ORF, metadata_simple, file = "rda/metadata.rda")
 
-# Create CSV subsets -----------------------------------------------------------
+# Create CSV subsets ===========================================================
 lapply(seq(along = datasets), function(i) {
   df <- mget(datasets.gsub[i], envir = .GlobalEnv)[[1]]
   file <- paste(c("csv/", datasets[i], ".csv"), collapse = "")
   write.csv(df, file)
 })
+system("gzip --force csv/*.csv")
 
-# Update RNAi library metadata -------------------------------------------------
+# Update RNAi library metadata =================================================
 # Be sure to run last!
 # Requires compiled metadata.rda to be saved prior
 #! source("ahringer.R", verbose = TRUE)
 #! source("cherrypick.R", verbose = TRUE)
 source("orfeome.R", verbose = TRUE)
-
-# gzip CSV files to save disk space --------------------------------------------
-system("gzip --force csv/*.csv")
+warnings()
--- orfeome.R
+++ orfeome.R
@@ -1,31 +1,26 @@
 pkg <- c("readxl")
 lapply(pkg, require, character.only = TRUE)
 load("rda/metadata.rda")
-
 # Import the Excel file ========================================================
 input <- read_excel("sources/orfeome.xlsx", sheet = 2)
 colnames(input) <- gsub(" ", ".", colnames(input))
-
 # Set up the data frame converted from Excel ===================================
-x <- data.frame()
-x <- input[, c("ORF.ID.(WS112)",
-               "Plate",
-               "Row",
-               "Col",
-               "RNAi.well",
-               "Nonv",
-               "Vpep",
-               "Simmer")]
-names(x)[names(x) == "ORF.ID.(WS112)"] <- "ORF"
+df <- data.frame()
+df <- input[, c("ORF.ID.(WS112)",
+                "Plate",
+                "Row",
+                "Col",
+                "RNAi.well")]
+names(df)[names(df) == "ORF.ID.(WS112)"] <- "ORF"
 # Subset the bad wells =========================================================
-x <- subset(x, !is.na(RNAi.well))
-x <- subset(x, ORF != "no match in WS112")
-input_subset <- x
-rm(x)
+df <- subset(df, !is.na(RNAi.well))
+df <- subset(df, ORF != "no match in WS112")
+input_subset <- df
+rm(df)
 # Set plate IDs as rownames ====================================================
-x <- input_subset
+df <- input_subset
 col <- c("Plate", "Row", "Col")
-ORFeomeID <- do.call(paste, c(x[col], sep = "-"))
+ORFeomeID <- do.call(paste, c(df[col], sep = "-"))
 rm(col)
 ORFeomeID[1]
 ORFeomeID <- gsub("^(.*)-([0-9]{1})$", "\\1-0\\2", ORFeomeID,
@@ -34,52 +29,47 @@ ORFeomeID[1]
 ORFeomeID <- gsub("^([0-9]{5})-([A-Z]{1})-([0-9]{2})$", "\\1@\\2\\3", ORFeomeID,
                   perl = TRUE, ignore.case = FALSE)
 ORFeomeID[1]
-x <- cbind(ORFeomeID, x)
-rownames(x) <- x$ORFeomeID
-orfeome_valid <- x
-rm(x, ORFeomeID)
+df <- cbind(ORFeomeID, df)
+rownames(df) <- df$ORFeomeID
+orfeome_valid <- df
+rm(df, ORFeomeID)
 # Get current metadata =========================================================
 # Since there are duplicate ORFs per well, we must loop from metadata_ORF
-x <- orfeome_valid
-orf <- as.vector(x$ORF)
+df <- orfeome_valid
+orf <- as.vector(df$ORF)
 list <- list()
 list <- lapply(seq(along = orf), function(i) {
   metadata_ORF[orf[i], ]
 })
 # Converting to a data frame here will take a while
-x <- data.frame(do.call("rbind", list))
-ORF_to_GeneID <- x
-rm(x, list, orf)
+df <- data.frame(do.call("rbind", list))
+ORF_to_GeneID <- df
+rm(df, list, orf)
 # Bind the matches back to the valid orfeome data frame ------------------------
-x <- data.frame()
-x <- cbind(ORF_to_GeneID, orfeome_valid)
-rownames(x) <- as.vector(x$ORFeomeID)
+df <- data.frame()
+df <- cbind(ORF_to_GeneID, orfeome_valid)
+rownames(df) <- as.vector(df$ORFeomeID)
 # Remove duplicate columns -----------------------------------------------------
 # This will only remove the first instance, here from ORF_to_GeneID
-x$ORF <- NULL
-x$ORFeomeID <- NULL
+df$ORF <- NULL
+df$ORFeomeID <- NULL
 # Remove unnecessary plate information
-x$Plate <- NULL
-x$Row <- NULL
-x$Col <- NULL
-colnames(x)
+df$Plate <- NULL
+df$Row <- NULL
+df$Col <- NULL
+colnames(df)
 # This will remove Plate, Row, Col
-x <- x[, c("RNAi.well",
-           "ORF",
-           "GeneID",
-           "public.name",
-           "gene.other.ids",
-           "Nonv",
-           "Vpep",
-           "Simmer")]
-orfeome <- x
-orfeome_simple <- x[, c("ORF", "GeneID", "public.name", "gene.other.ids")]
-rm(x)
-
+df <- df[, c("RNAi.well",
+             "ORF",
+             "GeneID",
+             "public.name",
+             "gene.other.ids")]
+orfeome <- df
+rm(df)
 # Additional information for library troubleshooting ===========================
 orfeome_unmatched <- orfeome[is.na(orfeome$GeneID), ]
 orfeome_unique <- unique(as.vector(orfeome$ORF))
-
 # Save =========================================================================
-save(orfeome, orfeome_simple, file = "rda/orfeome.rda")
+save(orfeome, file = "rda/orfeome.rda")
 write.csv(orfeome, "csv/orfeome.csv")
+system("gzip --force csv/orfeome.csv")
