934823ba2b455ec2a240ff93e11a1844341e06b6
Michael Steinbaugh <mike@steinbaugh.com>
2016-04-25 15:37:38 -0400

    Reorganize file structure

--- R/GeneID.R
+++ R/GeneID.R
@@ -1,22 +1,28 @@
 pkg <- c("readr", "stringr")
-lapply(pkg, require, character.only = TRUE)
+source("R/bioc_packages.R")
+
 # GeneID =======================================================================
-df <- read.csv("sources/geneIDs.txt.gz", header = FALSE, na.strings = "")
-df <- df[, c(2:5)] # discard uneeded columns
+df <- read_csv("source_data/wormbase/geneIDs.txt.gz",
+               col_names = FALSE,
+               na = "")
+# Discard uneeded columns
+df <- df[, c(2:5)]
 colnames(df) <- c("GeneID", "public.name", "ORF", "wormbase.status")
 rownames(df) <- df$GeneID
 GeneID_vec <- rownames(df)
 GeneID <- df
 rm(df)
+
 # Other IDs ====================================================================
-file <- read_file("sources/geneOtherIDs.txt.gz")
+file <- read_file("source_data/wormbase/geneOtherIDs.txt.gz")
 # Take out dead or live status, we have this from geneIDs.txt
 file <- gsub("\t(Dead|Live)", "", file, perl = TRUE)
 # Take the tabs out for gene list
 file <- gsub("\t", ", ", file)
 # Add tab back in to separate GeneID for row names
 file <- gsub("WBGene([0-9]+), ", "WBGene\\1\t", file, perl = TRUE)
-# Parsing failures here means there are no other IDs for that row
+# Warnings here mean there are no other IDs for that row
+# e.g. expected: 2 columns, actual: 1 columns
 df <- read_tsv(file, col_names = FALSE)
 rownames(df) <- df[, 1]
 colnames(df) <- c("GeneID", "gene.other.ids")
