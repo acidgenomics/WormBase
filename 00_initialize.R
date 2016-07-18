b549ad967366354243f2590484ffe73a2de49040
Michael Steinbaugh <mike@steinbaugh.com>
2016-07-18 07:35:12 -0400

    Switch to RMarkdown
    Former-commit-id: a2864c7e2164e0cbb9a8fbf9dbe4828cdae4888d
    Former-commit-id: e780489c58f9b6a5c5969ef76bbdb24bc66c7842

--- 00_initialize.R
+++ 00_initialize.R
@@ -1,11 +1,11 @@
 options(repos = "http://cran.rstudio.com/")
 
 install.packages("devtools")
+install.packages("roxygen2")
+
 install.packages("plyr")
-install.packages("R.utils")
 install.packages("readr")
 install.packages("readxl")
-install.packages("roxygen2")
 install.packages("stringr")
 
 source("https://bioconductor.org/biocLite.R")
