302fca99abb25d2eee5ff92f49b33c322eb711da
Michael Steinbaugh <mike@steinbaugh.com>
2016-08-17 18:37:14 -0400

    Use sourcedir function

--- data-raw/setup.R
+++ data-raw/setup.R
@@ -1,4 +1,3 @@
-library(dplyr)
-# Source all files in R folder
-sapply(list.files(pattern = "[.]R$", path = "R", full.names = TRUE), source)
 devtools::use_data_raw()
+library(dplyr)
+seqcloudr::sourcedir()
