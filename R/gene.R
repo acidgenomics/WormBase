6c023118463855f27b7609baffc190891c865c06
Michael Steinbaugh <mike@steinbaugh.com>
2016-08-19 16:52:44 -0400

    Version 0.5 update

--- R/gene.R
+++ R/gene.R
@@ -1,43 +1,44 @@
 #' Gene annotations
 #'
-#' @param rowNames Identifier vector
-#' @param id Identifier type (geneID, orf, publicName)
+#' @param id Identifier
+#' @param format Identifier type (geneID, orf, publicName)
 #' @param output Output type (report, simple)
 #'
 #' @return metadata data.frame
+#' @examples
+#' gene()
+#' gene(output = "simple")
+#' gene(id = c("WBGene00004804", "WBGene00001752"), format = "geneId")
+#' gene(id = c("T19E7.2", "K08F4.7"), format = "orf")
+#' gene(id = c("skn-1", "gst-4"), format = "publicName")
 #' @export
-gene <- function(rowNames = NULL, id = "geneId", output = "report") {
-  df <- geneInfo
-  rownames(df) <- df$geneId
+gene <- function(id = NULL, format = "geneId", output = "report") {
+  df <- geneData
+
+  # Strip ORF isoforms
+  if (format == "orf") {
+    id <- gsub("[a-z]{1}$", "", id)
+  }
+
+  # Subset if `id` declared
+  if (!is.null(id)) {
+    if (format == "geneId") {
+      df <- subset(df, df$geneId %in% id)
+    }
+    if (format == "orf") {
+      df <- subset(df, df$orf %in% id)
+    }
+    if (format == "publicName") {
+      df <- subset(df, df$publicName %in% id)
+    }
+  }
 
   # Subset columns
-  if (output == "simple") {
-    df <- df[, colNamesSimple]
-  } else {
+  if (output == "report") {
     df <- df[, colNamesReport]
   }
-
-  # orf matching
-  if (id == "orf") {
-    if (!is.null(rowNames)) {
-      # Strip isoforms from ORF
-      rowNames <- gsub("[a-z]{1}$", "", rowNames)
-    }
-    df <- subset(df, !is.na(df$orf))
-    df <- subset(df, !duplicated(df$orf))
-    rownames(df) <- df$orf
-  }
-
-  # publicName matching
-  if (id == "publicName") {
-    df <- subset(df, !is.na(df$publicName))
-    df <- subset(df, !duplicated(df$publicName))
-    rownames(df) <- df$publicName
-  }
-
-  # Subset rows
-  if (!is.null(rowNames)) {
-    df <- df[rowNames, ]
+  if (output == "simple") {
+    df <- df[, colNamesSimple]
   }
 
   return(df)
--- R/rnai.R
+++ R/rnai.R
@@ -1,39 +1,38 @@
 #' Feeding RNAi Library clone matching
 #'
-#' @param cloneId Clone identifier
-#' @param lib Feeding library (orfeome, ahringer)
-#' @param type Output type (simple, report)
+#' @param id Clone identifier
+#' @param library Feeding library (ahringer, orfeome)
+#' @param format Output type (report, simple)
 #'
 #' @return data.frame with metadata
+#' @examples
+#' rnai(library = "ahringer")
+#' rnai(id = "III-86@B01", library = "ahringer")
+#' rnai(library = "orfeome")
+#' rnai(id = "11010@G06", library = "orfeome")
 #' @export
-rnai <- function(cloneId = NULL,
-                 lib = "orfeome",
-                 type = "simple") {
-  if (lib == "ahringer") {
-    #! ADD GSUB FOR AHRINGER
-    df <- ahringerInfo
-  } else {
-    #! ADD GSUB FOR AHRINGER OR ORFEOME
-    df <- orfeomeInfo
+rnai <- function(id = NULL, library = "orfeome", format = "report") {
+  if (library == "ahringer") {
+    id <- gsub("^ahringer", "", id)
+    df <- rnaiData$ahringer
   }
-  rownames(df) <- df$cloneId
-
-  # Subset rows
-  rowNames <- cloneId
-  if (!is.null(rowNames)) {
-    df <- df[rowNames, ]
+  if (library == "orfeome") {
+    id <- gsub("^orfeome", "", id)
+    df <- rnaiData$orfeome
   }
 
-  df <- geneBind(df, id = "orf", type = type)
-
-  # Subset columns
-  if (type == "report") {
-    colNamesReport <- c("cloneId", colNamesReport)
-    df <- df[, colNamesReport]
-  } else {
-    colNamesSimple <- c("cloneId", colNamesSimple)
-    df <- df[, colNamesSimple]
+  if (!is.null(id)) {
+    df <- subset(df, df$cloneId %in% id)
   }
 
+  df <- merge(df, gene(df$orf, format = "orf"), by = "orf", all = TRUE)
+
+  if (format == "report") {
+    col <- c("cloneId", colNamesReport)
+  }
+  if (format == "simple") {
+    col <- c("cloneId", colNamesSimple)
+  }
+  df <- df[, col]
   return(df)
 }
