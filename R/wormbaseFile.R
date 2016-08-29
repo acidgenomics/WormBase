6e97d6ac4bda7d963138760037e0c42bbdf53dfb
Michael Steinbaugh <mike@steinbaugh.com>
2016-08-29 17:44:32 -0400

    Fix names
    Former-commit-id: 1c13ac1fddb1373dbdb55604a2f6d0b7029ba765
    Former-commit-id: cab4ee52d74e54eb16c81908a1c61fa7f401be23

--- R/wormbaseFile.R
+++ R/wormbaseFile.R
@@ -5,21 +5,15 @@
 #' @return Local file path of downloaded file.
 #' @export
 wormbaseFile <- function(request) {
-  annotation <-
-    "ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/"
+  annotation <- "ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/"
   version <- "canonical_bioproject.current"
-
-  if (remoteFile == "best_blast_hits") {
+  if (request == "best_blast_hits") {
     fileName <- "best_blastp_hits.txt.gz"
   } else {
-    fileName <- paste0(file, ".txt.gz")
+    fileName <- paste0(request, ".txt.gz")
   }
-  fileUrl <- paste0(annotation, file, "/c_elegans.", version, ".", fileName)
-
-  # data-raw method:
-  fileDir <- 
+  fileUrl <- paste0(annotation, request, "/c_elegans.", version, ".", fileName)
   filePath <- file.path("data-raw", "wormbase", fileName)
-
   if (!file.exists(filePath)) {
     utils::download.file(fileUrl, filePath)
   }
--- data-raw/geneData.R
+++ data-raw/geneData.R
@@ -3,9 +3,7 @@ library(seqcloudr)
 
 data(wormbase)
 wormbase <- Reduce(function(...) full_join(..., by = "geneId"), wormbase) %>%
-  select(-eValue) %>%
-  rename(hsapiensBlastpGeneId = ensemblGeneId,
-         hsapiensBlastpGeneName = ensemblGeneName)
+  select(-eValue)
 
 data(ensembl)
 ensembl <- Reduce(function(...) full_join(..., by = "ensembl_gene_id"), ensembl) %>%
@@ -17,6 +15,5 @@ data(panther)
 panther <- select(panther, -c(protein, subfamilyId))
 names(panther)[3:length(panther)] <- paste("panther", names(panther)[3:length(panther)], sep = "_") %>% camel
 
-geneData <- Reduce(function(...) left_join(..., by = "geneId"), list(wormbase, ensembl, panther)) %>%
-  cruft
+geneData <- Reduce(function(...) left_join(..., by = "geneId"), list(wormbase, ensembl, panther)) %>% cruft
 devtools::use_data(geneData, overwrite = TRUE)
