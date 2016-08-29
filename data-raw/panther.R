ce626348d4021331863b7a0dc82b9f04e6b7857c
Michael Steinbaugh <mike@steinbaugh.com>
2016-08-29 16:10:05 -0400

    pipe workflow

---
+++ data-raw/panther.R
@@ -0,0 +1,48 @@
+library(dplyr)
+library(magrittr)
+library(RCurl)
+library(readr)
+library(seqcloudr)
+library(stringr)
+library(tidyr)
+
+if (!file.exists("data-raw/panther.tsv")) {
+  file <- downloadFile("ftp://ftp.pantherdb.org/sequence_classifications/current_release/PANTHER_Sequence_Classification_files/",
+                       "nematode", rename = "panther.tsv")
+}
+panther <- read_tsv("data-raw/panther.tsv", col_names = FALSE) %>%
+  setNames(c("id",
+             "protein",
+             "subfamilyId",
+             "familyName",
+             "subfamilyName",
+             "geneOntologyMolecularFunction",
+             "geneOntologyBiologicalProcess",
+             "geneOntologyCellularComponent",
+             "class",
+             "pathway")) %>%
+  mutate(id = gsub("CAEEL\\|", "", id)) %>%
+  mutate(id = gsub("(EnsemblGenome|Gene|GeneID|UniProtKB|WormBase)=", "", id)) %>%
+  separate(id, c("geneId", "uniprotKb"), sep = "\\|") %>%
+  # Fix incorrect ID mappings:
+  mutate(geneId = gsub("^B0303.5$", "WBGene00015127", geneId)) %>%
+  mutate(geneId = gsub("^C13B9.1$", "WBGene00015732", geneId)) %>%
+  mutate(geneId = gsub("^F23F12.11/F23F12.5$", "WBGene00005075", geneId)) %>%
+  mutate(geneId = gsub("^F55A11.4$", "WBGene00010077", geneId)) %>%
+  mutate(geneId = gsub("^K02A2.6$", "WBGene00019291", geneId)) %>%
+  mutate(geneId = gsub("^MTCE.11$", "WBGene00010959", geneId)) %>%
+  mutate(geneId = gsub("^T07D3.8$", "WBGene00020310", geneId)) %>%
+  mutate(geneId = gsub("^Y104H12A.1/Y77E11A.5$", "WBGene00022423", geneId)) %>%
+  mutate(geneId = gsub("^Y62E10A.11$", "WBGene00014938", geneId)) %>%
+  mutate(geneId = gsub("^176090$", "WBGene00022794", geneId)) %>%  # ZK686.4
+  mutate(geneId = gsub("^2565696$", "WBGene00010966", geneId)) %>%  # MTCE.34
+  mutate(geneId = gsub("^2565700$", "WBGene00010964", geneId)) %>%  # MTCE.26
+  mutate(geneId = gsub("^2565701$", "WBGene00010962", geneId)) %>%  # MTCE.23
+  mutate(geneId = gsub("^2565702$", "WBGene00000829", geneId)) %>%  # MTCE.21
+  mutate(geneId = gsub("^2565703$", "WBGene00010967", geneId)) %>%  # MTCE.35
+  mutate(geneId = gsub("^2565704$", "WBGene00010960", geneId)) %>%  # MTCE.12
+  mutate(geneId = gsub("^2565705$", "WBGene00010963", geneId))  # MTCE.25
+devtools::use_data(panther, overwrite = TRUE)
+
+# Convert semicolon to double slash
+#! data.frame(apply(df, c(1, 2), function(x) gsub(";", " // ", x)))
