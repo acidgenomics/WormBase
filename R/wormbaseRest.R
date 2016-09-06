07472eb3fddec4ba990a39e07c31d70d9afc9823
Michael Steinbaugh <mike@steinbaugh.com>
2016-09-06 16:38:16 -0400

    roxygen update

--- R/wormbaseRest.R
+++ R/wormbaseRest.R
@@ -3,8 +3,6 @@
 #'
 #' @description
 #' \url{http://www.wormbase.org/about/userguide/for_developers/API-REST}
-#'
-#' @return
 wormbaseRest <- function(query, class, instance) {
     httr::GET(paste0("http://api.wormbase.org/rest/field/", class, "/", query, "/", instance),
               config = httr::content_type_json()) %>%
