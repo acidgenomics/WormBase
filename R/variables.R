05b08ca3297ef261169e38686ee66a0547d464a3
Michael Steinbaugh <mike@steinbaugh.com>
2016-09-12 09:24:44 -0400

    Weekend update

--- R/variables.R
+++ R/variables.R
@@ -5,30 +5,3 @@ utils::globalVariables(
       "geneId", "orf", "publicName",
       "ahringer96", "orfeome96", "sourceBioscience384")
 )
-
-colNamesSimple <-
-    c("geneId",
-      "orf",
-      "publicName")
-
-colNamesReport <-
-    c(colNamesSimple,
-      "geneOtherIds",
-      "geneClassDescription",
-      "conciseDescription",
-      "provisionalDescription",
-      "automatedDescription",
-      "hsapiensBlastpGeneName",
-      "hsapiensBlastpDescription",
-      "status",
-      "geneBiotype",
-      "geneOntologyName",
-      "geneOntologyId",
-      "interproDescription",
-      "interproId",
-      "pantherFamilyName",
-      "pantherSubfamilyName",
-      "pantherGeneOntologyMolecularFunction",
-      "pantherGeneOntologyBiologicalProcess",
-      "pantherGeneOntologyCellularComponent",
-      "pantherClass")
