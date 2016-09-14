2d9a2b6cbc051d3c3a5b585d883eac456bb61b65
Michael Steinbaugh <mike@steinbaugh.com>
2016-09-14 12:35:16 -0400

    cloneData update
    Former-commit-id: daa528a093d80adf98f4bd04fd80b6516da0356d
    Former-commit-id: acf73898444ebd13bc72183c27d4f88ad948961f

---
+++ data-raw/ahringer.R
@@ -0,0 +1,31 @@
+library(dplyr)
+library(readxl)
+library(seqcloudr)
+library(stringr)
+if (!file.exists("data-raw/ahringer.xlsx")) {
+    download.file("http://www.us.lifesciences.sourcebioscience.com/media/381254/C.%20elegans%20Database%202012.xlsx",
+                  "data-raw/ahringer.xlsx")
+}
+chromosomes <- c("I", "II", "III", "IV", "V", "X")
+list <- list()
+for (i in 1:length(chromosomes)) {
+    # Note that the first sheet contains notes, so \code{i + 1}
+    tbl <- readxl::read_excel("data-raw/ahringer.xlsx", sheet = i + 1, col_types = rep("text", 8)) %>%
+        magrittr::set_names(seqcloudr::camel(names(.))) %>%
+        dplyr::filter(!grepl("mismatch", extraInfo)) %>%
+        dplyr::select(-c(extraInfo, fwdPrimerSeq, revPrimerSeq)) %>%
+        dplyr::rename(genePair = genePairsName,
+                      ahringer384 = sourceBioscienceLocation) %>%
+        dplyr::mutate(plate = gsub("^S([0-9]{1})-", "S0\\1-", plate),
+                      ahringer96 = paste(stringr::str_pad(plate, 3, pad = "0"), well, sep = "-"),
+                      ahringer96 = gsub("^.*NA.*$", NA, ahringer96),
+                      ahringer384 = gsub("-([0-9}+)([A-Z]{1})", "-\\1-\\2", ahringer384),
+                      ahringer384 = gsub("-([0-9]{1})-", "-00\\1-", ahringer384),
+                      ahringer384 = gsub("-([0-9]{2})-", "-0\\1-", ahringer384)) %>%
+        dplyr::select(-c(plate, well, chrom))
+    name <- paste0("chr", chromosomes[i])
+    list[[i]] <- tbl
+}
+ahringer <- dplyr::bind_rows(list)
+rm(chromosomes, i, list, name, tbl)
+save(ahringer, file = "data-raw/ahringer.rda")
---
+++ data-raw/orfeome.R
@@ -0,0 +1,17 @@
+library(dplyr)
+library(readxl)
+library(seqcloudr)
+library(stringr)
+if (!file.exists("data-raw/orfeome.xlsx")) {
+    download.file("http://dharmacon.gelifesciences.com/uploadedFiles/Resources/cernai-feeding-library.xlsx",
+                  "data-raw/orfeome.xlsx")
+}
+orfeome <- readxl::read_excel("data-raw/orfeome.xlsx", sheet = 2) %>%
+    magrittr::set_names(seqcloudr::camel(names(.))) %>%
+    dplyr::rename(genePair = orfIdWs112) %>%
+    dplyr::select(plate, row, col, genePair) %>%
+    dplyr::filter(!grepl("no match", genePair)) %>%
+    dplyr::filter(!is.na(genePair)) %>%
+    dplyr::mutate(orfeome96 = paste0(plate, "-", row, stringr::str_pad(col, 2, pad = "0"))) %>%
+    dplyr::select(-c(plate, row, col))
+save(orfeome, file = "data-raw/orfeome.rda")
