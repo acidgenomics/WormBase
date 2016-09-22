e8caca7c6dc52a08a28c29fc8ee0c4dfa3b7cfe2
Michael Steinbaugh <mike@steinbaugh.com>
2016-09-22 15:43:13 -0400

    cleanup

--- docs/render.R
+++
@@ -1,7 +0,0 @@
-library(rmarkdown)
-rmarkdown::render("docs/README.Rmd",
-                  output_dir = ".",
-                  output_format = "github_document")
-rmarkdown::render("docs/data.Rmd",
-                  output_format = "md_document")
-
