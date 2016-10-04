8f71cb81d4e90f1467570b5c10763ec709f71dda
Michael Steinbaugh <mike@steinbaugh.com>
2016-10-04 10:27:11 -0400

    git lfs urls
    Former-commit-id: 8d252d69bc91b9caac8641c4f43381087f21a16e
    Former-commit-id: 20eb0bc6fa2a5bda37567e7c11ee6e0aa8e8b610

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,5 +1,5 @@
 # .onAttach <- function(libname, pkgname) {
-#     packageStartupMessage("Source data: https://github.com/steinbaugh/worminfo/tree/data")
+#     packageStartupMessage("Source data: https://github.com/steinbaugh/worminfo")
 # }
 
 #' @importFrom utils download.file
@@ -9,22 +9,22 @@
     # Download source data from the `data` branch on GitHub:
     # geneSource
     assign("geneSource", tempfile(), envir = envir)
-    utils::download.file("https://raw.githubusercontent.com/steinbaugh/worminfo/master/data/geneSource.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/geneSource.rda",
                          get("geneSource", envir = envir),
                          quiet = TRUE)
     load(get("geneSource", envir = envir), envir = envir)
 
     # rnaiSource
     assign("rnaiSource", tempfile(), envir = envir)
-    utils::download.file("https://raw.githubusercontent.com/steinbaugh/worminfo/master/data/rnaiSource.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/rnaiSource.rda",
                          get("rnaiSource", envir = envir),
                          quiet = TRUE)
     load(get("rnaiSource", envir = envir), envir = envir)
 
-
+    
     # Download the source data build information:
     assign("build", tempfile(), envir = envir)
-    utils::download.file("https://raw.githubusercontent.com/steinbaugh/worminfo/master/data/build.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/build.rda",
                          get("build", envir = envir),
                          quiet = TRUE)
     load(get("build", envir = envir), envir = envir)
