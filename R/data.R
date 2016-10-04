8d7331a25bb333a45ce5e285e730a948174e04bd
Michael Steinbaugh <mike@steinbaugh.com>
2016-10-04 10:48:22 -0400

    build fixes
    Former-commit-id: aa902f6474c0272351986d119578a587d9053156
    Former-commit-id: c43f000be4cb4e536b50799198f00046629c266e

--- R/data.R
+++ R/data.R
@@ -1,6 +1,27 @@
+# Needed for piping
+utils::globalVariables(c("."))
+
 # https://cran.r-project.org/web/packages/httr/vignettes/quickstart.html
 # https://cran.r-project.org/web/packages/httr/vignettes/api-packages.html
 user_agent <- httr::user_agent("https://github.com/steinbaugh/worminfo")
+
+
+# @importFrom utils download.file
+wormbaseFtpFile <- function(file) {
+    annotation <- "ftp://ftp.wormbase.org/pub/wormbase/species/c_elegans/annotation/"
+    version <- "canonical_bioproject.current"
+    if (file == "best_blast_hits") {
+        fileName <- "best_blastp_hits.txt.gz"
+    } else {
+        fileName <- paste0(file, ".txt.gz")
+    }
+    fileUrl <- paste0(annotation, file, "/c_elegans.", version, ".", fileName)
+    filePath <- file.path("data-raw", "wormbase", fileName)
+    if (!file.exists(filePath)) {
+        utils::download.file(fileUrl, filePath)
+    }
+    return(filePath)
+}
 
 
 # @import httr
