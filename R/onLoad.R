a9aae3b7a15fababebd87238e7256fede2d5bd2d
Michael Steinbaugh <mike@steinbaugh.com>
2016-10-04 12:33:47 -0400

    revert to data branch method
    Former-commit-id: 363c7916f0849fe59ebc821c0a4260d4a82a0451
    Former-commit-id: 17cafd2e60dc43ef10db7f7ebf23a39111fc3e8a

--- R/onLoad.R
+++ R/onLoad.R
@@ -9,14 +9,14 @@
     # Download source data from the `data` branch on GitHub:
     # geneSource
     assign("geneSource", tempfile(), envir = envir)
-    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/geneSource.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/data/data/geneSource.rda",
                          get("geneSource", envir = envir),
                          quiet = TRUE)
     load(get("geneSource", envir = envir), envir = envir)
 
     # rnaiSource
     assign("rnaiSource", tempfile(), envir = envir)
-    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/rnaiSource.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/data/data/rnaiSource.rda",
                          get("rnaiSource", envir = envir),
                          quiet = TRUE)
     load(get("rnaiSource", envir = envir), envir = envir)
@@ -24,7 +24,7 @@
     
     # Download the source data build information:
     assign("build", tempfile(), envir = envir)
-    utils::download.file("https://github.com/steinbaugh/worminfo/raw/master/data/build.rda",
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/data/data/build.rda",
                          get("build", envir = envir),
                          quiet = TRUE)
     load(get("build", envir = envir), envir = envir)
