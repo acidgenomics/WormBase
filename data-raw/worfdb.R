9c391644991a971aaecf47aa0fd4a31d0ebdc728
Michael Steinbaugh <mike@steinbaugh.com>
2016-12-13 16:46:12 -0500

    add functions

--- data-raw/worfdb.R
+++ data-raw/worfdb.R
@@ -1,11 +1,79 @@
-# WORFDB ====
+# Functions ====
+worfdbData <- function(worfdbHtml) {
+    pbmcapply::pbmclapply(seq_along(worfdbHtml), function(a) {
+        clone <- worfdbHtml[[a]] %>%
+            stringr::str_extract_all(., "GHR-[0-9]{5}@[A-H][0-9]+") %>%
+            unlist %>%
+            seqcloudr::toStringUnique(.)
+        inFrame <- worfdbHtml[[a]] %>%
+            stringr::str_extract_all(., "In Frame.+<font color=black>([NY])</font>") %>%
+            unlist %>%
+            stringr::str_replace(., "&nbsp;<font color=black>([NY])</font>", "\\1") %>%
+            stringr::str_replace_all(., "Y$", TRUE) %>%
+            stringr::str_replace_all(., "N$", FALSE) %>%
+            seqcloudr::toStringUnique(.)
+        sequence <- worfdbHtml[[a]] %>%
+            stringr::str_match_all(., "<A HREF=http://www.wormbase.org/db/seq/sequence\\?name=([A-Z0-9]+\\.[0-9]+[a-z]?)>") %>%
+            .[[1]] %>% .[, 2] %>%
+            seqcloudr::toStringUnique(.)
+        sequencingInformation <- worfdbHtml[[a]] %>%
+            stringr::str_extract_all(., "OST in ORFeome version.+\\(WS[0-9]+\\)") %>%
+            unlist %>%
+            seqcloudr::toStringUnique(.)
+        originalPosition <- worfdbHtml[[a]] %>%
+            stringr::str_match_all(., "Original Position: &nbsp;([0-9]{5}@[A-H][0-9]+)") %>%
+            .[[1]] %>% .[, 2] %>%
+            seqcloudr::toStringUnique(.)
+        primer <- worfdbHtml[[a]] %>%
+            stringr::str_match_all(., "<font color=red><B>([acgt]+)</B></font>") %>%
+            .[[1]] %>% .[, 2] %>%
+            toupper %>%
+            toString
+        size <- worfdbHtml[[a]] %>%
+            stringr::str_match_all(., "size: &nbsp;([0-9]+)") %>%
+            .[[1]] %>% .[, 2] %>%
+            toString
+        remap <- worfdbHtml[[a]] %>%
+            stringr::str_match_all(., "<TR><TD><A HREF=searchallwormorfs.pl\\?sid=([A-Z0-9]+\\.[0-9]+[a-z]?)>[A-Z0-9]+\\.[0-9]+[a-z]?</A></TD><TD>([0-9]{5}@[A-H][0-9]+)</TD><TD>([0-9]{5}@[A-H][0-9]+)?</TD><TD>(N|Y)</TD><TD>([0-9]+)</TD></TR>") %>%
+            .[[1]] %>% .[, 2] %>%
+            seqcloudr::toStringUnique(.)
+        list <- list(identifier = names(worfdbHtml)[a],
+                     sequence = sequence,
+                     clone = clone,
+                     originalPosition = originalPosition,
+                     sequencingInformation = sequencingInformation,
+                     inFrame = inFrame,
+                     primer = primer,
+                     size = size,
+                     remap = remap)
+        lapply(list, function(b) {
+            tibble::as_tibble(Filter(Negate(is.null), b))
+        })
+    }) %>%
+        dplyr::bind_rows(.) %>%
+        seqcloudr::wash(.)
+}
+
+worfdbHtml <- function(sequence) {
+    sequence <- sequence %>% na.omit %>% unique
+    pbmcapply::pbmclapply(seq_along(sequence), function(a) {
+        httr::GET(paste0("http://worfdb.dfci.harvard.edu/searchallwormorfs.pl?by=name&sid=",
+                         sequence[a]),
+                  user_agent = httr::user_agent(ua)) %>%
+            content("text")
+    }) %>% magrittr::set_names(sequence)
+}
+
+
+
+# Scrape by WormBase identifier ====
 if (!file.exists("data/worfdbHtml1.rda")) {
     data(wormbaseGene)
     sequence <- wormbaseGene$sequence
     worfdbHtml1 <- worfdbHtml(sequence)
     use_data(worfdbHtml1, overwrite = TRUE)
 } else {
-    data(worfdbHtml1)
+    data(worfdb1Html1)
 }
 
 worfdb1 <- worfdbData(worfdbHtml1)
@@ -32,14 +100,12 @@ worfdb <- bind_rows(worfdb1, worfdb2) %>%
 use_data(worfdb, overwrite = TRUE)
 rm(worfdb1, worfdb2)
 
-flag <- worfdb %>% filter(grepl("does not confirm", sequencingInformation))
 
 # Match to gene identifier with `gene()` function
-x <- worfdb %>%
+worfdb <- worfdb %>%
     select(-identifier) %>%
     filter(!is.na(sequence)) %>%
-    left_join(worminfo::gene(.$sequence,
-                             format = "sequence",
-                             select = "gene"),
+    left_join(gene(.$sequence, format = "sequence", select = "gene"),
               by = "sequence")
 
+flag <- worfdb %>% filter(grepl("does not confirm", sequencingInformation))
