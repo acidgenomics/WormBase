54de02dc845b01701a1d38b42e5098d4f17df99d
Michael Steinbaugh <mike@steinbaugh.com>
2016-12-13 16:52:50 -0500

    fix data loading and R folder ignore rules

--- R/onLoad.R
+++ R/onLoad.R
@@ -1,33 +1,33 @@
+#' @importFrom utils download.file
+.onLoad <- function(libname, pkgname) {
+    envir = asNamespace("worminfo")
+
+    # Download source data from the `data` branch on GitHub:
+    # geneAnnotations
+    assign("geneAnnotation", tempfile(), envir = envir)
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/geneAnnotation.rda",
+        get("geneAnnotation", envir = envir), quiet = TRUE)
+    load(get("geneAnnotation", envir = envir), envir = envir)
+
+    # rnaiAnnotations
+    assign("rnaiAnnotation", tempfile(), envir = envir)
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/rnaiAnnotation.rda",
+                         get("rnaiAnnotation", envir = envir),
+                         quiet = TRUE)
+    load(get("rnaiAnnotation", envir = envir), envir = envir)
+
+
+    # Download the source data build information:
+    assign("build", tempfile(), envir = envir)
+    utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/build.rda",
+                         get("build", envir = envir),
+                         quiet = TRUE)
+    load(get("build", envir = envir), envir = envir)
+}
+
 .onAttach <- function(libname, pkgname) {
     packageStartupMessage(
         paste(paste0("Annotations ", build$date, ":"),
               paste(build$wormbase, build$ensembl, build$panther, sep = ", "))
     )
 }
-
-# #' @importFrom utils download.file
-# .onLoad <- function(libname, pkgname) {
-#     envir = asNamespace("worminfo")
-#
-#     # Download source data from the `data` branch on GitHub:
-#     # geneAnnotations
-#     assign("geneAnnotation", tempfile(), envir = envir)
-#     utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/geneAnnotation.rda",
-#         get("geneAnnotation", envir = envir), quiet = TRUE)
-#     load(get("geneAnnotation", envir = envir), envir = envir)
-#
-#     # rnaiAnnotations
-#     assign("rnaiAnnotation", tempfile(), envir = envir)
-#     utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/rnaiAnnotation.rda",
-#                          get("rnaiAnnotation", envir = envir),
-#                          quiet = TRUE)
-#     load(get("rnaiAnnotation", envir = envir), envir = envir)
-#
-#
-#     # Download the source data build information:
-#     assign("build", tempfile(), envir = envir)
-#     utils::download.file("https://github.com/steinbaugh/worminfo/raw/dev/data/build.rda",
-#                          get("build", envir = envir),
-#                          quiet = TRUE)
-#     load(get("build", envir = envir), envir = envir)
-# }
