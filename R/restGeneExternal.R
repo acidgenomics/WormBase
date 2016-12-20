a0dbb61d3e1a6e57ea510401be65bbaab371cb5e
Michael Steinbaugh <mike@steinbaugh.com>
2016-12-20 16:42:43 -0500

    rework lapply loops in rest functions

--- R/restGeneExternal.R
+++ R/restGeneExternal.R
@@ -1,30 +1,28 @@
 #' WormBase RESTful gene external query
 #'
 #' @import dplyr
-#' @import httr
+#' @import magrittr
+#' @import parallel
 #' @import tibble
 #'
-#' @param gene Gene identifier
+#' @param identifier Gene identifier
 #'
 #' @return JSON content tibble
 #' @export
 #'
 #' @examples
 #' restGeneExternal("WBGene00000001")
-restGeneExternal <- function(gene) {
-    gene <- gene %>% unique %>% sort
-    list <- lapply(seq_along(gene), function(a) {
-        rest <- paste0("widget/gene/", gene[a], "/external_links") %>% rest %>%
-            .$fields %>% .$xrefs %>% .$data
-        list(gene = gene[a],
-             aceview = rest$AceView$gene$ids[[1]],
-             ncbi = rest$NCBI$gene$ids[[1]],
-             refseqMrna = rest$RefSeq$mRNA$ids[[1]],
-             refseqProtein = rest$RefSeq$protein$ids[[1]],
-             treefam = rest$TREEFAM$TREEFAM_ID$ids[[1]],
-             uniprot = rest$UniProt$UniProtAcc$ids[[1]])
-    })
-    dplyr::bind_rows(lapply(list, function(a) {
-        tibble::as_tibble(Filter(Negate(is.null), a))
-    }))
+restGeneExternal <- function(identifier) {
+    lapply(seq_along(identifier), function(a) {
+        b <- identifier[[a]]
+        rest <- paste0("widget/gene/", b, "/external_links") %>%
+            rest %>% .$fields %>% .$xrefs %>% .$data
+        parallel::mclapply(seq_along(rest), function(c) {
+            rest[[c]] %>% .[[1]] %>% .[[1]] %>%
+                unlist %>% toStringUnique
+        }) %>% magrittr::set_names(names(rest)) %>%
+            tibble::as_tibble(.) %>%
+            dplyr::mutate_(.dots = magrittr::set_names(list(~b), "gene"))
+    }) %>% dplyr::bind_rows(.) %>%
+        magrittr::set_names(tolower(names(.)))
 }
--- R/restGeneOntology.R
+++ R/restGeneOntology.R
@@ -3,29 +3,29 @@
 #' @import dplyr
 #' @import magrittr
 #' @import parallel
+#' @import tibble
 #'
-#' @param gene Gene identifier
+#' @param identifier Gene identifier
 #'
 #' @return JSON content tibble
 #' @export
 #'
 #' @examples
 #' restGeneOntology("WBGene00000001")
-restGeneOntology <- function(gene) {
-    lapply(seq_along(gene), function(w) {
-        x <- gene[[w]]
-        rest <- rest(paste0("widget/gene/", x, "/gene_ontology")) %>%
-            .$fields %>% .$gene_ontology %>% .$data
-        # Parallelize the recursive loops for each REST query:
-        parallel::mclapply(seq_along(rest), function(x) {
-            y <- rest[[x]]
-            lapply(seq_along(y), function(z) {
-                paste(identifier = y[[z]]$term_description$id,
-                      name = y[[z]]$term_description$label,
+restGeneOntology <- function(identifier) {
+    lapply(seq_along(identifier), function(a) {
+        b <- identifier[[a]]
+        rest <- paste0("widget/gene/", b, "/gene_ontology") %>%
+            rest %>% .$fields %>% .$gene_ontology %>% .$data
+        parallel::mclapply(seq_along(rest), function(c) {
+            d <- rest[[c]]
+            lapply(seq_along(d), function(e) {
+                paste(identifier = d[[e]]$term_description$id,
+                      name = d[[e]]$term_description$label,
                       sep = "~")
             }) %>% unique %>% toString
         }) %>% magrittr::set_names(names(rest)) %>%
             tibble::as_tibble(.) %>%
-            dplyr::mutate_(.dots = magrittr::set_names(list(~x), "gene"))
-    }) %>% dplyr::bind_rows(.)
+            dplyr::mutate_(.dots = magrittr::set_names(list(~b), "gene"))
+    }) %>% dplyr::bind_rows(.) %>% setNamesCamel
 }
