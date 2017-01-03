94a9b0d503afa92f77a7545c7b74c6eeb0dcf625
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-03 11:44:13 -0500

    fixed function to match multiple cogFunctionalCategory letters

--- R/eggnog.R
+++ R/eggnog.R
@@ -1,7 +1,7 @@
 #' EggNOG annotations
 #'
 #' @export
-#' @importFrom dplyr left_join select_
+#' @importFrom dplyr left_join rename_ select_
 #' @param query EggNOG identifier
 #' @return tibble
 #'
@@ -11,11 +11,15 @@ eggnog <- function(query) {
     annotation <- get("eggnogAnnotation", envir = asNamespace("worminfo"))
     category <- get("eggnogCategory", envir = asNamespace("worminfo"))
     query <- query %>% stats::na.omit(.) %>% unique %>% sort
-    annotation %>%
-        .[.$groupName %in% query, ] %>%
-        dplyr::left_join(category, by = "cogFunctionalCategory") %>%
-        dplyr::select_(.dots = c("groupName",
-                                 "cogFunctionalCategory",
-                                 "cogFunctionalDescription",
-                                 "consensusFunctionalDescription"))
+    annotation <- annotation %>%
+        .[.$groupName %in% query,
+          c("groupName",
+            "consensusFunctionalDescription",
+            "cogFunctionalCategory")] %>%
+        dplyr::rename_(.dots = c("eggnog" = "groupName"))
+    category <- category %>%
+        .[.$cogFunctionalCategory %in% strsplit(annotation$cogFunctionalCategory, "")[[1]], ] %>%
+        collapse
+    category$cogFunctionalCategory <- gsub(", ", "", category$cogFunctionalCategory)
+    dplyr::left_join(annotation, category, by = "cogFunctionalCategory")
 }
