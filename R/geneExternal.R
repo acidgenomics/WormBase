a0dd3cb6e7ae7cd2f21c190032c189c1b75c88d2
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-03 16:16:10 -0500

    simplify geneExternal

--- R/geneExternal.R
+++ R/geneExternal.R
@@ -13,24 +13,20 @@
 #'
 #' @examples
 #' geneExternal("WBGene00000001")
-geneExternal <- function(identifier, all = FALSE) {
-    result <- lapply(seq_along(identifier), function(a) {
-        b <- identifier[[a]]
-        if (!grepl("^WBGene[0-9]{8}$", b)) {
+geneExternal <- function(identifier) {
+    identifier <- identifier %>% stats::na.omit(.) %>% unique %>% sort
+    lapply(seq_along(identifier), function(a) {
+        if (!grepl("^WBGene[0-9]{8}$", identifier[[a]])) {
             stop("Invalid gene identifier.")
         }
-        rest <- paste0("widget/gene/", b, "/external_links") %>%
+        rest <- paste0("widget/gene/", identifier[[a]], "/external_links") %>%
             rest %>% .$fields %>% .$xrefs %>% .$data
-        parallel::mclapply(seq_along(rest), function(c) {
-            rest[[c]] %>% .[[1]] %>% .[[1]] %>%
+        parallel::mclapply(seq_along(rest), function(b) {
+            rest[[b]] %>% .[[1]] %>% .[[1]] %>%
                 unlist %>% toStringUnique
         }) %>% magrittr::set_names(names(rest)) %>%
             tibble::as_tibble(.) %>%
-            dplyr::mutate_(.dots = magrittr::set_names(list(~b), "gene"))
+            dplyr::mutate_(.dots = magrittr::set_names(list(~identifier[[a]]), "gene"))
     }) %>% dplyr::bind_rows(.) %>%
         magrittr::set_names(tolower(names(.)))
-    if (!isTRUE(all)) {
-        result <- result[, c("gene", "ncbi", "uniprot")]
-    }
-    return(result)
 }
