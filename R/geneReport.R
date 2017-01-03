6d6587524b75918b691c617f892d7840572c5e0e
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-03 14:48:49 -0500

    improved column order

--- R/geneReport.R
+++ R/geneReport.R
@@ -1,48 +1,49 @@
 #' Gene list report
 #'
 #' @export
-#' @importFrom dplyr left_join
-#' @param query Gene identifier
+#' @importFrom dplyr arrange_ left_join select_
+#' @param identifier Gene identifier
 #' @return tibble
-#'
-#' @examples
-#' geneReport("WBGene00000001")
-geneReport <- function(query) {
-    report <- gene(query, select = c(simpleCol,
-                                     "class",
-                                     "biotype",
-                                     # Ortholog:
-                                     "blastpHsapiensGene",
-                                     "blastpHsapiensName",
-                                     "blastpHsapiensDescription",
-                                     "orthologHsapiens",
-                                     # Description:
-                                     "descriptionConcise",
-                                     "descriptionProvisional",
-                                     "descriptionAutomated",
-                                     "ensemblDescription",
-                                     # WormBase Additional:
-                                     "rnaiPhenotype",
-                                     # Gene Ontology:
-                                     "ensemblGeneOntology",
-                                     "interpro",
-                                     "pantherFamilyName",
-                                     "pantherSubfamilyName",
-                                     "pantherGeneOntologyMolecularFunction",
-                                     "pantherGeneOntologyBiologicalProcess",
-                                     "pantherGeneOntologyCellularComponent",
-                                     "pantherClass")) %>%
-        dplyr::left_join(geneOntology(.$gene), by = "gene") %>%
-        dplyr::left_join(geneExternal(.$gene), by = "gene") %>%
-        dplyr::left_join(uniprot(.$gene), by = "gene")
-    report$groupName <- report$eggnog %>%
-        stringr::str_extract(., "ENOG41[A-Z0-9]{5}")
-    report <- dplyr::left_join(report,
-                               eggnog(x$groupName),
-                               by = "groupName")
-    report$groupName <- NULL
-    return(report)
-}
+geneReport <- function(identifier) {
+    gene <- gene(identifier,
+                 select = c(simpleCol,
+                            "class",
+                            "biotype",
+                            # Ortholog:
+                            "blastpHsapiensGene",
+                            "blastpHsapiensName",
+                            "blastpHsapiensDescription",
+                            "orthologHsapiens",
+                            # Description:
+                            "descriptionConcise",
+                            "descriptionProvisional",
+                            "descriptionAutomated",
+                            "ensemblDescription",
+                            # WormBase Additional:
+                            "rnaiPhenotype",
+                            # Gene Ontology:
+                            "ensemblGeneOntology",
+                            "interpro",
+                            "pantherFamilyName",
+                            "pantherSubfamilyName",
+                            "pantherGeneOntologyMolecularFunction",
+                            "pantherGeneOntologyBiologicalProcess",
+                            "pantherGeneOntologyCellularComponent",
+                            "pantherClass"))
+    geneOntology <- geneOntology(identifier)
+    geneExternal <- geneExternal(identifier)
+    uniprot <- uniprot(identifier)
+    eggnog <- uniprot$eggnog %>%
+        strsplit(", ") %>% .[[1]] %>%
+        eggnog %>%
+        collapse
 
-# reorder columns, sort by eggNog category then UniProt score confidence
-# Write wormbaseCitation function -- PMID identifiers
+    gene %>%
+        dplyr::left_join(geneExternal, by = "gene") %>%
+        dplyr::left_join(geneOntology, by = "gene") %>%
+        dplyr::left_join(uniprot, by = "gene") %>%
+        dplyr::left_join(eggnog, by = "eggnog") %>%
+        dplyr::select_(.dots = c("gene",
+                                 setdiff(sort(names(.)), "gene"))) %>%
+        dplyr::arrange_(.dots = c("gene"))
+}
