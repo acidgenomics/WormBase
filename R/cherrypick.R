5ec8fe76a9f2bed91763a89d09b048a1c80ce558
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-08 18:20:00 -0500

    simplify

--- R/cherrypick.R
+++ R/cherrypick.R
@@ -4,12 +4,8 @@
 #' @importFrom dplyr arrange_ right_join
 #' @param keyword Keyword
 #' @return RNAi clone list by gene
-#'
-#' @examples
-#' cherrypick("transcription")
 cherrypick <- function(keyword) {
     gene(keyword, format = "keyword") %>%
-        dplyr::right_join(rnai(.$gene, format = "gene"),
-                          by = c("gene", "sequence", "name")) %>%
+        dplyr::right_join(rnai(.$gene, format = "gene"), by = simpleCol) %>%
         dplyr::arrange_(.dots = names(.))
 }
