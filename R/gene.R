11d7e54b6b10c03358fc8ed775666523503082a4
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-08 13:49:37 -0500

    Better selection filter and sorting of gene and class format

--- R/gene.R
+++ R/gene.R
@@ -1,19 +1,16 @@
 #' Gene mapping
 #'
+#' @export
 #' @importFrom dplyr arrange_ bind_rows
 #' @importFrom parallel mclapply
 #' @importFrom stats na.omit
 #' @importFrom tidyr separate_
-#'
 #' @param identifier Identifier
 #' @param format Identifier type (\code{gene}, \code{name}, \code{sequence},
 #'   \code{class} or \code{keyword})
 #' @param select Columns to select. Consult the \code{gene} vignette for
 #'   available parameters.
-#' @param sort Columns to use for sorting.
-#'
 #' @return tibble
-#' @export
 #'
 #' @examples
 #' gene("skn-1", format = "name")
@@ -21,86 +18,79 @@
 #' gene("WBGene00004804", format = "gene", select = "descriptionConcise")
 #' gene("daf", format = "class")
 #' gene("bzip", format = "keyword")
-gene <- function(identifier,
-                 format = "gene",
-                 select = NULL,
-                 sort = NULL) {
+gene <- function(identifier, format = "gene", select = NULL) {
     if (missing(identifier)) {
         stop("An identifier is required.")
     } else if (!is.character(identifier)) {
         stop("Identifier must be a character vector.")
     }
+    identifier <- identifier %>% stats::na.omit(.) %>% unique
     annotation <- get("geneAnnotation", envir = asNamespace("worminfo"))
-    identifier <- identifier %>% stats::na.omit(.) %>% unique %>% sort
-    data <- parallel::mclapply(seq_along(identifier), function(a) {
-        # Format ====
+    return <- parallel::mclapply(seq_along(identifier), function(a) {
         if (any(grepl(format, c("gene", "name")))) {
-            data <- annotation %>%
+            return <- annotation %>%
                 .[.[[format]] %in% identifier[a], ]
         } else if (format == "sequence") {
             # Strip out isoform if necessary
             gsub <- gsub("^([A-Z0-9]+)\\.([0-9]+)[a-z]$", "\\1.\\2", identifier[a])
-            data <- annotation %>% .[.[[format]] %in% gsub, ]
-            if (nrow(data)) {
-                data$sequence <- identifier[a]
-            }
+            return <- annotation %>% .[.[[format]] %in% gsub, ]
         } else if (format == "class") {
             name <- annotation %>%
                 .[grepl(paste0("^", identifier[a], "-"), .[["name"]]), "name"]
             name <- name[[1]]
-            data <- annotation %>% .[.$name %in% name, ]
+            return <- annotation %>% .[.$name %in% name, ]
         } else if (format == "keyword") {
-            # `apply(..., 1)` processes by row:
+            # \code{apply(..., 1)} processes by row:
             grepl <- apply(annotation, 1, function(b) {
                 any(grepl(identifier[a], b, ignore.case = TRUE))
             })
             gene <- annotation[grepl, "gene"]
             gene <- gene[[1]]
-            data <- annotation %>% .[.$gene %in% gene, ]
-            if (nrow(data)) {
-                data$keyword <- identifier[a]
-            }
+            return <- annotation %>% .[.$gene %in% gene, ]
         } else {
             stop("Invalid format.")
         }
-        return(data)
+        if (nrow(return)) {
+            return[[format]] <- identifier[a]
+        }
+        return(return)
     }) %>% dplyr::bind_rows(.)
 
+    # Collapse multiple keyword matches:
+    if (format == "keyword") {
+        return <- return %>%
+            dplyr::group_by_(.dots = "gene") %>%
+            collapse
+    }
 
-    # Select ====
+    # Select columns:
+    # Always return the WormBase gene identifier.
     if (is.null(select)) {
-        data <- data[, simpleCol]
+        return <- return[, unique(c(format, simpleCol))]
     } else {
-        data <- data[, unique(c(format, select))]
+        return <- return[, unique(c(format, "gene", select))]
     }
 
+    # Put \code{format} column first:
+    return <- return %>%
+        dplyr::select_(.dots = c(format, setdiff(names(.), format)))
 
-    # Sort ====
-    # `format` is used for sorting (except `keyword`), unless specified:
-    if (format == "class") {
-        sort <- "name"
-    } else if (is.null(sort) && format != "keyword") {
-        sort <- format
+    # Arrange rows:
+    # \code{format} is used to arrange, unless specified.
+    if (any(grepl(format, c("class", "name")))) {
+        return$temp <- return$name
+        # Split to temporary columns:
+        return <- tidyr::separate_(return, "temp",
+                                   into = c("tempPrefix", "tempNum"),
+                                   sep = "-")
+        return$tempNum <- as.numeric(return$tempNum)
+        # Arrange by class then number:
+        return <- dplyr::arrange_(return, .dots = c("tempPrefix", "tempNum"))
+        # Drop the unnecessary temporary columns:
+        return$tempPrefix <- NULL
+        return$tempNum <- NULL
+    } else {
+        return <- dplyr::arrange_(return, .dots = unique(format, "gene"))
     }
-    if (!is.null(sort)) {
-        if (sort[1] == "name") {
-            data$temp <- data$name
-            # Split to temporary columns for proper sorting later:
-            data <- tidyr::separate_(data, "temp",
-                                     into = c("tempPrefix", "tempNum"),
-                                     sep = "-")
-            data$tempNum <- as.numeric(data$tempNum)
-            # Sort by class then number:
-            data <- dplyr::arrange_(data, .dots = c("tempPrefix", "tempNum"))
-            # Drop the unnecessary temporary columns:
-            data$tempPrefix <- NULL
-            data$tempNum <- NULL
-        } else {
-            if (!all(sort %in% names(data))) {
-                stop("Sort parameters are invalid.")
-            }
-            data <- dplyr::arrange_(data, .dots = sort)
-        }
-    }
-    return(data)
+    return(return)
 }
