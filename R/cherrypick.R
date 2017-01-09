2da38ef890d009a2f2ade0025322c96010d42f2c
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-09 16:18:04 -0500

    check identifier

--- R/cherrypick.R
+++ R/cherrypick.R
@@ -1,11 +1,12 @@
 #' Cherrypick RNAi clones by keyword
-#'
 #' @export
 #' @importFrom dplyr arrange_ right_join
-#' @param keyword Keyword
+#' @param identifier Keyword identifier
 #' @return RNAi clone list by gene
-cherrypick <- function(keyword) {
-    gene(keyword, format = "keyword") %>%
-        dplyr::right_join(rnai(.$gene, format = "gene"), by = simpleCol) %>%
+cherrypick <- function(identifier) {
+    identifier %>%
+        uniqueIdentifier %>%
+        gene(format = "keyword") %>%
+        dplyr::right_join(rnai(.$gene, format = "gene"), by = defaultCol) %>%
         dplyr::arrange_(.dots = names(.))
 }
