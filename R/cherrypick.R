567b359fcaa4b09412d7e350d7d64c56c387b5f5
Michael Steinbaugh <mike@steinbaugh.com>
2017-01-11 13:23:15 -0500

    draft update

--- R/cherrypick.R
+++ R/cherrypick.R
@@ -2,11 +2,19 @@
 #' @export
 #' @importFrom dplyr arrange_ right_join
 #' @param identifier Keyword identifier
+#' @param format Identifier format
+#' @param controlWells Number of control wells per cherrypick plate
 #' @return RNAi clone list by gene
-cherrypick <- function(identifier) {
+cherrypick <- function(identifier,
+                       format = "keyword",
+                       controlWells = 3) {
     identifier %>%
         uniqueIdentifier %>%
-        gene(format = "keyword") %>%
+        gene(format = format) %>%
         dplyr::right_join(rnai(.$gene, format = "gene"), by = defaultCol) %>%
         dplyr::arrange_(.dots = names(.))
 }
+
+# See Natalie's cherrypick for data example
+# cherrypick plate identifiers
+# control wells: default = 3
