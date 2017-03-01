0e36606f64581f9be83282a1cfa227d292eef803
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-01 15:04:16 -0500

    Placeholder import calls

---
+++ R/basejump.R
@@ -0,0 +1,60 @@
+#' @importFrom basejump camel
+camel <- function(a) {
+    basejump::camel(a)
+}
+
+
+
+#' @importFrom basejump dataRaw
+dataRaw <- function(a) {
+    basejump::dataRaw(a)
+}
+
+
+
+#' @importFrom basejump fixNA
+fixNA <- function(a) {
+    basejump::fixNA(a)
+}
+
+
+
+#' @importFrom basejump grepToString
+grepToString <- function(a) {
+    basejump::grepToString(a)
+}
+
+
+
+#' @importFrom basejump markdownList
+markdownList <- function(a) {
+    basejump::markdownList(a)
+}
+
+
+
+#' @importFrom basejump setNamesCamel
+setNamesCamel <- function(a) {
+    basejump::setNamesCamel(a)
+}
+
+
+
+#' @importFrom basejump toStringSortUnique
+toStringSortUnique <- function(a) {
+    basejump::toStringSortUnique(a)
+}
+
+
+
+#' @importFrom basejump toStringSummarize
+toStringSummarize <- function(a) {
+    basejump::toStringSummarize(a)
+}
+
+
+
+#' @importFrom basejump toStringUnique
+toStringUnique <- function(a) {
+    basejump::toStringUnique(a)
+}
