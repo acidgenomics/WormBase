8225c83dccb3350e39d7263b4cf1dd90cff3911f
Michael Steinbaugh <mike@steinbaugh.com>
2017-03-02 00:29:37 -0500

    Consolidate into RMarkdown workflows

--- data-raw/annotation.R
+++
@@ -1,5 +0,0 @@
-dataRaw(c("build", "eggnog", "gene", "rnai"))
-annotation <- list(gene = gene,
-                   rnai = rnai,
-                   eggnog = eggnog)
-use_data(annotation, overwrite = TRUE)
