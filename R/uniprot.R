355cc698c004ef1286cac664bad3f8577ac032a2
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:26:31 -0400

    Update documentation and remove explicit calls

--- R/uniprot.R
+++ R/uniprot.R
@@ -1,46 +1,45 @@
 #' UniProt web service query
 #'
-#' @export
-#' @importFrom dplyr bind_rows group_by_ rename_ select_
-#' @importFrom tidyr nest
-#' @importFrom UniProt.ws select UniProt.ws
 #' @param identifier WormBase gene identifier
+#'
 #' @return tibble
+#' @export
 uniprot <- function(identifier) {
     identifier <- uniqueIdentifier(identifier)
-    database <- UniProt.ws::UniProt.ws(taxId = 6239)  # NCBI C. elegans
-    uniprot <-  UniProt.ws::select(database,
-                                   keytype = "WORMBASE",
-                                   keys = identifier,
-                                   columns = c("EGGNOG",
-                                               "EXISTENCE",
-                                               "FAMILIES",
-                                               "GO",
-                                               "KEYWORDS",
-                                               "REVIEWED",
-                                               "SCORE",
-                                               "UNIPROTKB",
-                                               "WORMBASE"))
+    database <- UniProt.ws(taxId = 6239)  # NCBI C. elegans
+    uniprot <-  UniProt.ws::select(
+        database,
+        keytype = "WORMBASE",
+        keys = identifier,
+        columns = c("EGGNOG",
+                    "EXISTENCE",
+                    "FAMILIES",
+                    "GO",
+                    "KEYWORDS",
+                    "REVIEWED",
+                    "SCORE",
+                    "UNIPROTKB",
+                    "WORMBASE"))
     if (nrow(uniprot)) {
         uniprot <- uniprot %>%
             setNamesCamel %>%
-            dplyr::select_(.dots = c("wormbase",
-                                     setdiff(sort(names(.)), "wormbase")))
+            select_(.dots = c("wormbase",
+                              setdiff(sort(names(.)), "wormbase")))
         eggnog <- eggnog(uniprot$eggnog)
-        x <- dplyr::left_join(uniprot, eggnog, by = "eggnog") %>%
+        x <- left_join(uniprot, eggnog, by = "eggnog") %>%
             # Sort priority to put higher quality UniProtKB identifiers first:
             .[order(.$wormbase,
                     .$cogFunctionalDescription,
                     -xtfrm(.$score)), ] %>%
-            dplyr::rename_(.dots = c("gene" = "wormbase",
-                                     "uniprotExistence" = "existence",
-                                     "uniprotFamilies" = "families",
-                                     "uniprotGeneOntology" = "go",
-                                     "uniprotKeywords" = "keywords",
-                                     "uniprotReviewed" = "reviewed",
-                                     "uniprotScore" = "score")) %>%
-            dplyr::select_(.dots = sort(names(.))) %>%
-            dplyr::group_by_(.dots = "gene") %>%
+            rename_(.dots = c("gene" = "wormbase",
+                              "uniprotExistence" = "existence",
+                              "uniprotFamilies" = "families",
+                              "uniprotGeneOntology" = "go",
+                              "uniprotKeywords" = "keywords",
+                              "uniprotReviewed" = "reviewed",
+                              "uniprotScore" = "score")) %>%
+            select_(.dots = sort(names(.))) %>%
+            group_by_(.dots = "gene") %>%
             toStringSummarize
     }
 }
