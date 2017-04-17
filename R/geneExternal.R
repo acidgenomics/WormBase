a56128cbcbc33efee3ecc1d006e257b1dd71195c
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:25:35 -0400

    Update documentation and remove explicit calls

--- R/geneExternal.R
+++ R/geneExternal.R
@@ -1,25 +1,26 @@
 #' WormBase RESTful gene external query
-#' @export
-#' @importFrom dplyr bind_rows mutate_
-#' @importFrom magrittr set_names
-#' @importFrom parallel mclapply
-#' @importFrom tibble as_tibble
+#'
 #' @param identifier Gene identifier
+#'
 #' @return JSON content tibble
+#' @export
 geneExternal <- function(identifier) {
     identifier <- uniqueIdentifier(identifier)
     lapply(seq_along(identifier), function(a) {
         if (!grepl("^WBGene[0-9]{8}$", identifier[[a]])) {
             stop("Invalid gene identifier.")
         }
-        rest <- paste0("widget/gene/", identifier[[a]], "/external_links") %>%
+        rest <- file.path("widget/gene",
+                          identifier[[a]],
+                          "external_links") %>%
             rest %>% .$fields %>% .$xrefs %>% .$data
-        parallel::mclapply(seq_along(rest), function(b) {
+        mclapply(seq_along(rest), function(b) {
             rest[[b]] %>% .[[1]] %>% .[[1]] %>%
                 unlist %>% toStringUnique
-        }) %>% magrittr::set_names(names(rest)) %>%
-            tibble::as_tibble(.) %>%
-            dplyr::mutate_(.dots = magrittr::set_names(list(~identifier[[a]]), "gene"))
-    }) %>% dplyr::bind_rows(.) %>%
-        magrittr::set_names(tolower(names(.)))
+        }) %>% setNames(names(rest)) %>%
+            as_tibble %>%
+            mutate_(.dots = setNames(list(
+                ~identifier[[a]]), "gene"))
+    }) %>% bind_rows %>%
+        setNames(tolower(names(.)))
 }
