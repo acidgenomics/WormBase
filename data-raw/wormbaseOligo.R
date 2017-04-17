133572fed144bc5d1b5ca13c7358b105626b4d49
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:48:13 -0400

    Move scripts out of data-raw

--- data-raw/wormbaseOligo.R
+++
@@ -1,4 +0,0 @@
-wormbaseOligo <- wormbaseAnnotationFile("pcr_product2gene") %>%
-    read_tsv(col_names = c("oligo", "gene")) %>%
-    mutate(gene = str_extract(gene, "WBGene\\d{8}"))
-use_data(wormbaseOligo, overwrite = TRUE)
