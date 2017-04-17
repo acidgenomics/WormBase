d4e4cf6c348b2dcb802883e0ea0509b38abaf178
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:25:59 -0400

    Update documentation and remove explicit calls

--- R/geneOntology.R
+++ R/geneOntology.R
@@ -1,31 +1,32 @@
 #' WormBase RESTful RNAi gene ontology query
-#' @export
-#' @importFrom dplyr bind_rows mutate_ rename_
-#' @importFrom magrittr set_names
-#' @importFrom parallel mclapply
-#' @importFrom tibble as_tibble
+#'
 #' @param identifier Gene identifier
+#'
 #' @return JSON content tibble
+#' @export
 geneOntology <- function(identifier) {
     identifier <- uniqueIdentifier(identifier)
     lapply(seq_along(identifier), function(a) {
         if (!grepl("^WBGene[0-9]{8}$", identifier[[a]])) {
-            stop("Invalid gene identifier.")
+            stop("Invalid gene identifier")
         }
-        rest <- paste0("widget/gene/", identifier[[a]], "/gene_ontology") %>%
+        rest <- file.path("widget/gene",
+                          identifier[[a]],
+                          "gene_ontology") %>%
             rest %>% .$fields %>% .$gene_ontology %>% .$data
         if (!is.null(rest)) {
-            parallel::mclapply(seq_along(rest), function(b) {
+            mclapply(seq_along(rest), function(b) {
                 lapply(seq_along(rest[[b]]), function(c) {
                     paste(identifier = rest[[b]][[c]]$term_description$id,
                           name = rest[[b]][[c]]$term_description$label,
                           sep = "~")
                 }) %>% unique %>% toString
-            }) %>% magrittr::set_names(camel(paste0("wormbaseGeneOntology_", names(rest)))) %>%
-                tibble::as_tibble(.) %>%
-                dplyr::mutate_(.dots = magrittr::set_names(list(~identifier[[a]]), "gene"))
+            }) %>% setNames(camel(paste0("wormbaseGeneOntology_",
+                                         names(rest)))) %>%
+                as_tibble %>%
+                mutate_(.dots = setNames(list(~identifier[[a]]), "gene"))
         } else {
-            tibble::tibble()
+            tibble()
         }
-    }) %>% dplyr::bind_rows(.)
+    }) %>% bind_rows
 }
