3df3099178d2156d0805c30b05b8df29529e3942
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:23:44 -0400

    Update documentation and remove explicit calls

--- R/cherrypick.R
+++ R/cherrypick.R
@@ -1,40 +1,41 @@
 #' Cherrypick RNAi clones by keyword
-#' @export
-#' @importFrom dplyr arrange_ right_join
-#' @importFrom stats setNames
-#' @importFrom tidyr unnest
+#'
 #' @param identifier Keyword identifier
 #' @param format Identifier format
 #' @param ahringer384 Include Ahringer 384 well library
 #' @param ahringer96 Include Ahringer 96 well library
 #' @param orfeome96 Include ORFeome 96 well library
 #' @param plates Reference character of plates to pick from
+#'
 #' @return RNAi clone list by gene
-cherrypick <- function(identifier,
-                       format = "keyword",
-                       ahringer384 = TRUE,
-                       ahringer96 = FALSE,
-                       orfeome96 = TRUE,
-                       plates = NULL) {
-    return <- identifier %>%
+#' @export
+cherrypick <- function(
+    identifier,
+    format = "keyword",
+    ahringer384 = TRUE,
+    ahringer96 = FALSE,
+    orfeome96 = TRUE,
+    plates = NULL) {
+    df <- identifier %>%
         uniqueIdentifier %>%
         gene(format = format) %>%
-        dplyr::right_join(rnai(.$gene, format = "gene"), by = defaultCol) %>%
-        dplyr::mutate_(.dots = stats::setNames(list(quote(strsplit(clone, ", "))), "clone")) %>%
-        tidyr::unnest(.) %>%
-        dplyr::arrange_(.dots = "clone")
+        right_join(rnai(.$gene, format = "gene"), by = defaultCol) %>%
+        mutate_(.dots = setNames(list(
+            quote(strsplit(clone, ", "))), "clone")) %>%
+        unnest %>%
+        arrange_(.dots = "clone")
     if (!isTRUE(ahringer384)) {
-        return <- return[!grepl("^ahringer384", return$clone), ]
+        df <- df[!grepl("^ahringer384", df$clone), ]
     }
     if (!isTRUE(ahringer96)) {
-        return <- return[!grepl("^ahringer96", return$clone), ]
+        df <- df[!grepl("^ahringer96", df$clone), ]
     }
     if (!isTRUE(orfeome96)) {
-        return <- return[!grepl("^orfeome96", return$clone), ]
+        df <- df[!grepl("^orfeome96", df$clone), ]
     }
     if (!is.null(plates)) {
         grep <- paste0("^(", paste(plates, collapse = "|"), ")-\\D\\d{2}$")
-        return <- return[grepl(grep, return$clone), ]
+        df <- df[grepl(grep, df$clone), ]
     }
-    return
+    return(df)
 }
