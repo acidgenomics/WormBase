db9f97c1d06e3d52c743d0b233e4220a7e2ce597
Michael Steinbaugh <mike@steinbaugh.com>
2017-04-17 14:38:47 -0400

    Update documentation and reduce explicit calls

--- R/rnai.R
+++ R/rnai.R
@@ -1,15 +1,12 @@
 #' RNAi clone mapping
 #'
-#' @export
-#' @importFrom dplyr bind_rows left_join
-#' @importFrom parallel mclapply
-#' @importFrom stats na.omit
 #' @param identifier Identifier
 #' @param format Identifier format (\code{clone}, \code{gene}, \code{genePair},
 #'   \code{name}, or \code{sequence})
-#' @param proteinCoding Whether to only return protein coding matches
-#'   (\code{TRUE}, \code{FALSE})
+#' @param proteinCoding Return protein coding matches (\code{TRUE/FALSE})
+#'
 #' @return tibble
+#' @export
 #'
 #' @examples
 #' rnai("ahringer384-III-6-C01")
@@ -20,46 +17,47 @@
 #' rnai("WBGene00004735", format = "gene")
 #' rnai("Y47D3B.7", format = "sequence")
 #' rnai("Y53H1C.b", format = "genePair")
-rnai <- function(identifier,
-                 format = "clone",
-                 proteinCoding = TRUE) {
+rnai <- function(
+    identifier,
+    format = "clone",
+    proteinCoding = TRUE) {
     identifier <- uniqueIdentifier(identifier)
     grep <- identifier
     annotation <- get("annotation", envir = asNamespace("worminfo"))$rnai
     if (!any(grepl(format, names(annotation)))) {
-        stop("Invalid format.")
+        stop("invalid format")
     }
     if (format == "clone") {
         grep <- grep %>%
-            # Strip prefixes:
+            # Strip prefixes
             gsub("^GHR", "", .) %>%
             gsub("^([a-z]+)(96|384)", "", .) %>%
-            # Strip separators:
+            # Strip separators
             gsub("-|@", "", .) %>%
-            # Strip padded zeroes:
+            # Strip padded zeroes
             gsub("(^|-)[0]+", "", .) %>%
             gsub("([A-Z]{1})[0]+(\\d)$", "\\1\\2", .)
     } else if (format == "sequence") {
         grep <- removeIsoform(grep)
     }
-    # Now create the grep string:
+    # Now create the grep string
     grep <- grep %>% grepToString
-    return <- parallel::mclapply(seq_along(grep), function(a) {
+    return <- mclapply(seq_along(grep), function(a) {
         return <- annotation %>% .[grepl(grep[a], .[[format]]), ]
         if (nrow(return)) {
             if (format != "clone") {
-                # Sort the clones and make human readable:
+                # Sort the clones and make human readable
                 return$clone <- return$clone %>%
                     strsplit(", ") %>% .[[1]] %>%
-                    # Pad well numbers:
+                    # Pad well numbers
                     gsub("(\\D)(\\d)$", "\\10\\2", .) %>%
-                    # Plate separator:
+                    # Plate separator
                     gsub("(\\d+)(\\D\\d{2})$", "-\\1-\\2", .) %>%
-                    # ORFeome 96 well:
+                    # ORFeome 96 well
                     gsub("^-(\\d{5})-", "orfeome96-\\1-", .) %>%
-                    # Ahringer 384 well:
+                    # Ahringer 384 well
                     gsub("^([IVX]{1,3})-", "ahringer384-\\1-", .) %>%
-                    # Ahringer 96 well:
+                    # Ahringer 96 well
                     gsub("^-(\\d{1,3})-", "ahringer96-\\1-", .) %>%
                     # Present only `ahringer96` and `orfeome96` clones to user.
                     # Other identifiers (`ahringer96`, `cherrypick`) are for
@@ -70,11 +68,12 @@ rnai <- function(identifier,
             return[[format]] <- identifier[a]
         }
         return
-    }) %>% dplyr::bind_rows(.)
+    }) %>% bind_rows
     if (nrow(return)) {
         if (isTRUE(proteinCoding)) {
-            return <- dplyr::filter_(return, .dots = quote(biotype == "protein_coding"))
+            return <- filter_(return, .dots = quote(
+                biotype == "protein_coding"))
         }
-        dplyr::select_(return, .dots = unique(c(format, defaultCol, "clone")))
+        select_(return, .dots = unique(c(format, defaultCol, "clone")))
     }
 }
