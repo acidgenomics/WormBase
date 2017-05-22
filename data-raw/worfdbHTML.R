167f89213a3d8bdc204bf010a24fbed069c80251
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-21 23:36:51 -0400

    WORFDB resave

---
+++ data-raw/worfdbHTML.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:973346ccd4fa50106f221daa9aaea9e4ca7c1619fb84c1069090b6d880045d7a
+size 85
---
+++ data-raw/worfdbHTMLRemap.R
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:d6dd208adba7d716ca944f76269c484ebbf522616955f2c7792d65cb89717c27
+size 233
