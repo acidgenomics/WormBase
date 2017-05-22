fecc23c831829301bc17687e8b0ae8cb7d616829
Michael Steinbaugh <mike@steinbaugh.com>
2017-05-22 08:10:04 -0400

    Draft update -- fixes needed

--- R/uniprot.R
+++ R/uniprot.R
@@ -1,14 +1,14 @@
 #' UniProt web service query
 #'
-#' @author Michael Steinbaugh
+#' @param identifier WormBase gene identifier.
 #'
-#' @param identifier WormBase gene identifier
-#'
-#' @return tibble
+#' @return Tibble.
 #' @export
 uniprot <- function(identifier) {
+    # [fix] Unknown or uninitialised column: 'cogFunctionalCategory'.
     identifier <- uniqueIdentifier(identifier)
-    database <- UniProt.ws(taxId = 6239)  # NCBI C. elegans
+    # NCBI C. elegans identifier = 6239
+    database <- UniProt.ws(taxId = 6239)
     uniprot <-  UniProt.ws::select(
         database,
         keytype = "WORMBASE",
@@ -24,25 +24,28 @@ uniprot <- function(identifier) {
                     "WORMBASE"))
     if (nrow(uniprot)) {
         uniprot <- uniprot %>%
-            setNamesCamel %>%
-            select_(.dots = c("wormbase",
-                              setdiff(sort(names(.)), "wormbase")))
+            camel %>%
+            select(!!!syms(c(
+                "wormbase", setdiff(sort(names(.)), "wormbase"))))
         eggnog <- eggnog(uniprot$eggnog)
         # Check and make sure this output is correct
         left_join(uniprot, eggnog, by = "eggnog") %>%
             # Sort priority to put higher quality UniProtKB identifiers first
+            # [fix] rework using dplyr method -- top_n?
             .[order(.$wormbase,
                     .$cogFunctionalDescription,
                     -xtfrm(.$score)), ] %>%
-            rename_(.dots = c("gene" = "wormbase",
-                              "uniprotExistence" = "existence",
-                              "uniprotFamilies" = "families",
-                              "uniprotGeneOntology" = "go",
-                              "uniprotKeywords" = "keywords",
-                              "uniprotReviewed" = "reviewed",
-                              "uniprotScore" = "score")) %>%
-            select_(.dots = sort(names(.))) %>%
-            group_by_(.dots = "gene") %>%
-            toStringSummarize
+            # [fix] check that this works
+            rename(gene = !!sym("wormbase"),
+                   uniprotExistence = !!sym("existence"),
+                   uniprotFamilies = !!sym("families"),
+                   uniprotGeneOntology = !!sym("go"),
+                   uniprotKeywords = !!sym("keywords"),
+                   uniprotReviewed = !!sym("reviewed"),
+                   uniprotScore = !!sym("score")) %>%
+            # [fix] this may error out
+            select(sort(names(.))) %>%
+            group_by(!!sym("gene")) %>%
+            summarizeRows
     }
 }
