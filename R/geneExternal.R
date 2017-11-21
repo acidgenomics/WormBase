746fd6e0bea9d07076b9b53f343b788ca3b701c8
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-21 10:20:41 -0500

    Rework flow and add function imports

--- R/geneExternal.R
+++ R/geneExternal.R
@@ -1,6 +1,8 @@
-#' WormBase RESTful gene external query
+#' WormBase RESTful Gene External Query
 #'
-#' @author Michael Steinbaugh
+#' @importFrom basejump toStringUnique
+#' @importFrom dplyr bind_rows
+#' @importFrom parallel mclapply
 #'
 #' @param identifier Gene identifier.
 #'
@@ -8,34 +10,35 @@
 #' @export
 #'
 #' @examples
-#' geneExternal("WBGene00000001") %>% glimpse
+#' geneExternal("WBGene00000001") %>% glimpse()
 geneExternal <- function(identifier) {
     identifier <- uniqueIdentifier(identifier)
-    lapply(seq_along(identifier), function(a) {
-        if (!grepl("^WBGene[0-9]{8}$", identifier[[a]])) {
-            stop("Invalid gene identifier.")
+    list <- lapply(seq_along(identifier), function(a) {
+        if (!grepl(pattern = "^WBGene[0-9]{8}$", x = identifier[[a]])) {
+            stop("Invalid gene identifier")
         }
-        rest <- file.path("widget/gene",
-                          identifier[[a]],
-                          "external_links") %>%
-            rest %>%
+        rest <- file.path(
+            "widget",
+            "gene",
+            identifier[[a]],
+            "external_links") %>%
+            rest() %>%
             .[["fields"]] %>%
             .[["xrefs"]] %>%
             .[["data"]]
-        mclapply(seq_along(rest), function(b) {
+        xrefs <- mclapply(seq_along(rest), function(b) {
             rest[[b]] %>%
                 .[[1L]] %>%
                 .[[1L]] %>%
-                unlist %>%
-                toStringUnique
-        }
-        ) %>%
-            set_names(names(rest)) %>%
-            as_tibble %>%
-            # FIXME check that this works
+                unlist() %>%
+                toStringUnique()
+        })
+        names(xrefs) <- names(rest)
+        xrefs %>%
+            as_tibble() %>%
             mutate(gene = identifier[[a]])
-    }
-    ) %>%
-        bind_rows %>%
-        set_names(tolower(names(.)))
+    })
+    df <- bind_rows(list)
+    names(df) <- tolower(names(df))
+    df
 }
