dcf9cd16efa01b8bd1d32a68fcbdd67580aaa5f3
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-21 10:21:18 -0500

    Add imports

--- R/microplate.R
+++ R/microplate.R
@@ -1,17 +1,20 @@
-#' Generate well identifiers for a microplate
+#' Generate Well Identifiers for a Microtiter Plate
+#'
+#' @importFrom rlang is_string
+#' @importFrom stringr str_length str_pad
 #'
 #' @param plate Number of plates.
 #' @param well Number of wells (`96`, `384`).
 #' @param control Number of control wells.
 #' @param prefix *Optional*. Plate name prefix.
 #'
-#' @return Data frame.
+#' @return [data.frame].
 #' @export
 microplate <- function(
     plate = 1L,
     well = 96L,
     control = 0L,
-    prefix = NULL) {
+    prefix) {
     if (!is.numeric(plate) | plate < 1L) {
         stop("Invalid plate identifier")
     }
@@ -24,11 +27,14 @@ microplate <- function(
     } else {
         stop("Invalid plate format")
     }
-    col <- 1L:col %>% str_pad(max(str_length(.)), pad = "0")
+    col <- 1L:col %>%
+        str_pad(max(str_length(.)), pad = "0")
     row <- LETTERS[1L:row]
-    plate <- 1L:plate %>% str_pad(max(str_length(.)), pad = "0")
+    plate <- 1L:plate %>%
+        str_pad(max(str_length(.)), pad = "0")
     df <- expand.grid(plate, row, col)
-    vector <- paste0(df[["Var1"]], "-", df[["Var2"]], df[["Var3"]]) %>% sort
+    vector <- paste0(df[["Var1"]], "-", df[["Var2"]], df[["Var3"]]) %>%
+        sort()
     # Remove control wells from vector:
     if (!is.numeric(control) | !control %in% 0L:12L) {
         stop("Please specify 0:12 control wells")
@@ -43,10 +49,10 @@ microplate <- function(
         vector <- vector[!grepl(grep, vector)]
     }
     # Add a prefix, if desired:
-    if (length(prefix) != 1L) {
-        stop("Only a single character prefix is allowed")
-    }
-    if (!is.null(prefix) & is.character(prefix)) {
+    if (!missing(prefix)) {
+        if (!is_string(prefix)) {
+            stop("Prefix must be a string")
+        }
         vector <- paste0(prefix, "-", vector)
     }
     return(vector)
