29733f5e0e9adc1d93d9ecade42e9624db673a5c
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-21 10:22:49 -0500

    Update imports, specifying them at the top of each function where applicable

--- R/worminfo-package.R
+++ R/worminfo-package.R
@@ -1,12 +1,6 @@
 #' worminfo
 #'
 #' C. elegans genome annotations and RNAi clone mappings.
-#'
-#' @import basejump
-#' @import Biobase
-#' @import BiocGenerics
-#' @import S4Vectors
-#' @importFrom UniProt.ws UniProt.ws
 "_PACKAGE"
 
 globalVariables(".")
