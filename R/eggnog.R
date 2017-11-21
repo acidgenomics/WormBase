3e6c06bca2ada91c49c96f4225190f2f0a15558b
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-21 09:25:55 -0500

    Update with explicit package imports

--- R/eggnog.R
+++ R/eggnog.R
@@ -1,8 +1,11 @@
-#' EggNOG annotations
+#' EggNOG Annotations
+#'
+#' @importFrom basejump summarizeRows
+#' @importFrom dplyr bind_rows left_join distinct
 #'
 #' @param identifier EggNOG identifier.
 #'
-#' @return Tibble.
+#' @return [tibble].
 #' @export
 eggnog <- function(identifier) {
     identifier <- uniqueIdentifier(identifier)
@@ -20,20 +23,26 @@ eggnog <- function(identifier) {
         categoryMatch <- lapply(seq_along(
             annotationMatch[["cogFunctionalCategory"]]), function(a) {
             letter <- annotationMatch[["cogFunctionalCategory"]][a] %>%
-                # FIXME?
-                str_split("") %>%
-                unlist %>%
-                sort %>%
-                unique
+                strsplit("") %>%
+                unlist() %>%
+                sort() %>%
+                unique()
             category %>%
                 .[.[["cogFunctionalCategory"]] %in% letter, ] %>%
-                summarizeRows
+                # FIXME summarizeRows isn't exported anymore
+                summarizeRows()
         }
         ) %>%
-            bind_rows %>%
-            distinct
+            bind_rows() %>%
+            distinct()
         categoryMatch[["cogFunctionalCategory"]] <-
-            gsub(", ", "", categoryMatch[["cogFunctionalCategory"]])
-        left_join(annotationMatch, categoryMatch, by = "cogFunctionalCategory")
+            gsub(pattern = ", ",
+                 replacement = "",
+                 x = categoryMatch[["cogFunctionalCategory"]])
+        annotationMatch <- left_join(
+            annotationMatch,
+            categoryMatch,
+            by = "cogFunctionalCategory")
     }
+    annotationMatch
 }
