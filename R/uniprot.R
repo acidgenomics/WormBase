ff4332542196a39688423e300c643d667596abd2
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-29 13:14:31 -0500

    Improve `uniprot()` return and add working example

--- R/uniprot.R
+++ R/uniprot.R
@@ -3,18 +3,26 @@
 #' UniProt Web Service Query
 #'
 #' @importFrom basejump camel collapseToString
-#' @importFrom dplyr left_join rename select
+#' @importFrom dplyr left_join pull rename
 #' @importFrom rlang !! sym
 #' @importFrom UniProt.ws UniProt.ws
+#'
+#' @keywords internal
 #'
 #' @param identifier WormBase gene identifier.
 #'
 #' @return [tibble].
 #' @export
+#'
+#' @examples
+#' # daf-2
+#' uniprot("WBGene00000898") %>%
+#'     glimpse()
 uniprot <- function(identifier) {
     identifier <- .uniqueIdentifier(identifier)
+
     # NCBI C. elegans identifier = 6239
-    database <- UniProt.ws(taxId = 6239L)
+    database <- UniProt.ws::UniProt.ws(taxId = 6239L)
     # Explicitly call the `AnnotationDbi::select()`` generic here, avoiding
     # collision with `dplyr::select()`.
     uniprot <- UniProt.ws::select(
@@ -30,26 +38,31 @@ uniprot <- function(identifier) {
                     "SCORE",
                     "UNIPROTKB",
                     "WORMBASE"))
-    if (nrow(uniprot)) {
-        uniprot <- uniprot %>%
-            camel() %>%
-            select(c("wormbase", setdiff(sort(names(.)), "wormbase")))
-        eggnog <- eggnog(uniprot[["eggnog"]])
-        # Check and make sure this output is correct
-        left_join(uniprot, eggnog, by = "eggnog") %>%
-            # Sort priority to put higher quality UniProtKB identifiers first
-            .[order(.[["wormbase"]],
-                    .[["cogFunctionalDescription"]],
-                    -xtfrm(.[["score"]])), ] %>%
-            rename(gene = !!sym("wormbase"),
-                   uniprotExistence = !!sym("existence"),
-                   uniprotFamilies = !!sym("families"),
-                   uniprotGeneOntology = !!sym("go"),
-                   uniprotKeywords = !!sym("keywords"),
-                   uniprotReviewed = !!sym("reviewed"),
-                   uniprotScore = !!sym("score")) %>%
-            select(sort(names(.))) %>%
-            group_by(!!sym("gene")) %>%
-            collapseToString()
+    if (!nrow(uniprot)) {
+        return(NULL)
     }
+
+    uniprot <- uniprot %>%
+        camel() %>%
+        .[, c("wormbase", setdiff(sort(names(.)), "wormbase")), ]
+    eggnog <- pull(uniprot, "eggnog") %>%
+        eggnog()
+
+    left_join(uniprot, eggnog, by = "eggnog") %>%
+        # Sort priority to put higher quality UniProtKB identifiers first
+        .[order(.[["wormbase"]],
+                -xtfrm(.[["score"]]),
+                .[["reviewed"]],
+                .[["cogFunctionalDescription"]]), ] %>%
+        rename(
+            gene = .data[["wormbase"]],
+            uniprotExistence = .data[["existence"]],
+            uniprotFamilies = .data[["families"]],
+            uniprotGeneOntology = .data[["go"]],
+            uniprotKeywords = .data[["keywords"]],
+            uniprotReviewed = .data[["reviewed"]],
+            uniprotScore = .data[["score"]]
+        ) %>%
+        group_by(!!sym("gene")) %>%
+        collapseToString(sort = FALSE, unique = TRUE)
 }
