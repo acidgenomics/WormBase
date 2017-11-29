d46e0ea9df06e6c884bc36b04c2f3d5c6828e46c
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-29 13:17:50 -0500

    Prefix function with a dot

--- R/internal-rest.R
+++ R/internal-rest.R
@@ -1,5 +1,5 @@
 #' @importFrom httr content content_type_json GET user_agent
-rest <- function(url) {
+.rest <- function(url) {
     file.path("http://api.wormbase.org", "rest", url) %>%
         GET(config = content_type_json(),
             user_agent = user_agent(userAgent)) %>%
