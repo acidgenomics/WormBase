90f993f51e65d520d523bf4c5ff75c9ea9741533
Michael Steinbaugh <mike@steinbaugh.com>
2017-11-29 11:45:12 -0500

    Simplify the `cherrypick()` function. Remove the `plates` argument and simply return clones from all supported libraries as columns.

--- R/cherrypick.R
+++ R/cherrypick.R
@@ -1,49 +1,25 @@
 #' Cherrypick RNAi Clones by Keyword
 #'
-#' @importFrom dplyr arrange mutate right_join
+#' @importFrom dplyr arrange everything mutate pull right_join select
 #' @importFrom rlang !! sym
 #' @importFrom tidyr unnest
 #'
 #' @param identifier Keyword identifier.
 #' @param format Identifier format.
-#' @param ahringer384 Include Ahringer 384 well library.
-#' @param ahringer96 Include Ahringer 96 well library.
-#' @param orfeome96 Include ORFeome 96 well library.
-#' @param plates *Optional*. Character vector of reference plates (for
-#'   subsetting).
 #'
 #' @return RNAi clone [list] by gene.
 #' @export
 cherrypick <- function(
     identifier,
-    format = "keyword",
-    ahringer384 = TRUE,
-    ahringer96 = FALSE,
-    orfeome96 = TRUE,
-    plates) {
-    df <- .uniqueIdentifier(identifier) %>%
-        gene(format = format) %>%
-        right_join(
-            rnai(.[["gene"]], format = "gene"),
-            by = defaultCol) %>%
-        mutate(clone = strsplit(.data[["clone"]], ", ")) %>%
-        unnest() %>%
-        arrange(!!sym("clone"))
-    if (!isTRUE(ahringer384)) {
-        df <- df[!grepl("^ahringer384", df[["clone"]]), ]
+    format = "keyword") {
+    identifier <- .uniqueIdentifier(identifier)
+    gene <- gene(identifier, format = format)
+    if (nrow(gene) == 0) {
+        return(NULL)
     }
-    if (!isTRUE(ahringer96)) {
-        df <- df[!grepl("^ahringer96", df[["clone"]]), ]
-    }
-    if (!isTRUE(orfeome96)) {
-        df <- df[!grepl("^orfeome96", df[["clone"]]), ]
-    }
-    if (!missing(plates)) {
-        if (!is.character(plates)) {
-            stop("Plates must be a character vector")
-        }
-        grep <- paste0("^(", paste(plates, collapse = "|"), ")-\\D\\d{2}$")
-        df <- df[grepl(grep, df[["clone"]]), ]
-    }
-    return(df)
+    rnai <- pull(gene, "gene") %>%
+        rnai(format = "gene")
+    rnai[["genePair"]] <- NULL
+    df <- left_join(rnai, gene, by = defaultCol)
+    select(df, unique(c(format, defaultCol)), everything())
 }
