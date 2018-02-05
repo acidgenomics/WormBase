2657e4239bb4a65aec486a2f7206b0d0f34ef154
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-05 07:44:46 -0500

    Update unit tests

--- tests/testthat/test_geneReport.R
+++ tests/testthat/test_geneReport.R
@@ -2,49 +2,50 @@ context("geneReport")
 
 test_that("geneReport", {
     data <- geneReport("skn-1", format = "name")
-    expect_equal(
-        colnames(data),
-        c("gene",
-          "sequence",
-          "name",
-          "class",
-          "ortholog",
-          "descriptionConcise",
-          "rnaiPhenotype",
-          "pantherSubfamilyName",
-          "pantherGoMF",
-          "pantherGoBP",
-          "pantherGoCC",
-          "pantherClass",
-          "pantherPathway",
-          "ensemblDescription",
-          "biotype",
-          "broadClass",
-          "geneSeqStart",
-          "geneSeqEnd",
-          "seqName",
-          "seqStrand",
-          "seqCoordSystem",
-          "entrez",
-          "biologicalProcess",  # prefix with wormbase
-          "cellularComponent",  # prefix with wormbase
-          "molecularFunction",  # prefix with wormbase
-          "eggnog",
-          "uniprotExistence",
-          "uniprotFamilies",
-          "uniprotGeneOntology",
-          "uniprotKeywords",
-          "uniprotReviewed",
-          "uniprotScore",
-          "uniprotkb",  # rename to uniprotKb or uniprotKB?
-          "consensusFunctionalDescription",  # prefix with eggnog
-          "cogFunctionalCategory",  # prefix with eggnog
-          "cogFunctionalDescription")
+    expect_identical(
+        lapply(data, class),
+        list(
+            "gene" = "character",
+            "sequence" = "character",
+            "name" = "character",
+            "class" = "character",
+            "ortholog" = "list",
+            "descriptionConcise" = "character",
+            "rnaiPhenotype" = "list",
+            "pantherSubfamilyName" = "character",
+            "pantherGoMF" = "list",
+            "pantherGoBP" = "list",
+            "pantherGoCC" = "list",
+            "pantherClass" = "list",
+            "pantherPathway" = "list",
+            "ensemblDescription" = "character",
+            "biotype" = "character",
+            "broadClass" = "character",
+            "geneSeqStart" = "integer",
+            "geneSeqEnd" = "integer",
+            "seqName" = "character",
+            "seqStrand" = "integer",
+            "seqCoordSystem" = "character",
+            "entrez" = "list",
+            "biologicalProcess" = "character",  # wormbase
+            "cellularComponent" = "character",  # wormbase
+            "molecularFunction" = "character",  # wormbase
+            "eggnog" = "character",
+            "uniprotExistence" = "character",
+            "uniprotFamilies" = "character",
+            "uniprotGeneOntology" = "character",
+            "uniprotKeywords" = "character",
+            "uniprotReviewed" = "character",
+            "uniprotScore" = "character",
+            "uniprotkb" = "character",  # rename to uniprotKb or uniprotKB?
+            "consensusFunctionalDescription" = "character",  # eggnog
+            "cogFunctionalCategory" = "character",  # eggnog
+            "cogFunctionalDescription" = "character"
+        )
     )
-
 })
 
-test_that("bad identifier", {
+test_that("Invalid identifier", {
     expect_equal(
         geneReport("XXX"),
         NULL
