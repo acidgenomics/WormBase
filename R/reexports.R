5794b798d183f3837d7e837a74d78ebcfa2af3d4
Michael Steinbaugh <mike@steinbaugh.com>
2018-02-14 06:42:27 -0500

    Update NAMESPACE for vignette

--- R/reexports.R
+++ R/reexports.R
@@ -1,10 +1,18 @@
+#' @importFrom basejump markdownList
+#' @export
+basejump::markdownList
+
 #' @importFrom basejump microplate
 #' @export
 basejump::microplate
 
+
+
 #' @importFrom magrittr %>%
 #' @export
 magrittr::`%>%`
+
+
 
 #' @importFrom tibble glimpse
 #' @export
