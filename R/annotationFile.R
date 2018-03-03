4180aad7c75d8a5bd8eff525d52b045fe4c83629
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 17:23:04 -0500

    Add support for versioned releases

---
+++ R/annotationFile.R
@@ -0,0 +1,86 @@
+#' Annotation File
+#'
+#' @importFrom basejump initializeDirectory
+#' @importFrom fs path
+#' @importFrom utils download.file
+#'
+#' @param file Annotation file name.
+#' @param version WormBase version. If `NULL`, defaults to current production
+#'   release version available on the WormBase website. Legacy versions can
+#'   be specified as a string (e.g. "WS262").
+#' @param dir Output directory.
+#'
+#' @return Silently return the file path.
+#' @export
+#'
+#' @examples
+#' # Current
+#' current <- annotationFile("geneIDs")
+#' print(current)
+#'
+#' # Versioned
+#' versioned <- annotationFile("geneIDs", version = "WS262")
+#' print(versioned)
+#'
+#' # Clean up
+#' file_exists(c(current, versioned))
+#' file_delete(c(current, versioned))
+annotationFile <- function(
+    file,
+    version = NULL,
+    dir = "."
+) {
+    assert_is_a_string(file)
+    dir <- initializeDirectory(dir)
+    if (is_a_string(version)) {
+        assert_all_are_matching_regex(version, versionPattern)
+        fileName <- paste(
+            "c_elegans",
+            bioproject,
+            version,
+            file,
+            "txt",
+            "gz",
+            sep = "."
+        )
+        url <- paste(
+            "ftp://ftp.wormbase.org",
+            "pub",
+            "wormbase",
+            "releases",
+            version,
+            "species",
+            "c_elegans",
+            bioproject,
+            "annotation",
+            fileName,
+            sep = "/"
+        )
+    } else {
+        fileName <- paste(
+            "c_elegans",
+            "canonical_bioproject",
+            "current",
+            file,
+            "txt",
+            "gz",
+            sep = "."
+        )
+        url <- paste(
+            "ftp://ftp.wormbase.org",
+            "pub",
+            "wormbase",
+            "species",
+            "c_elegans",
+            bioproject,
+            "annotation",
+            file,
+            fileName,
+            sep = "/"
+        )
+    }
+    destfile <- path(dir, fileName)
+    names(destfile) <- file
+    download.file(url = url, destfile = destfile)
+    invisible(destfile)
+}
