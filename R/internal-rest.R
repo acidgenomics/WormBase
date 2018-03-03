2fde7f17764bd893e85051d1cf099b16b3482ccc
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 17:52:15 -0500

    Export the `rest()` function

--- R/internal-rest.R
+++
@@ -1,7 +0,0 @@
-#' @importFrom httr content content_type_json GET user_agent
-.rest <- function(url) {
-    file.path("http://api.wormbase.org", "rest", url) %>%
-        GET(config = content_type_json(),
-            user_agent = user_agent(userAgent)) %>%
-        content()
-}
---
+++ R/rest.R
@@ -0,0 +1,14 @@
+#' RESTful API Query
+#'
+#' @importFrom httr content content_type_json GET user_agent
+#'
+#' @return REST query results.
+#' @export
+rest <- function(query) {
+    x <- GET(
+        url = paste("http://api.wormbase.org", "rest", query, sep = "/"),
+        config = content_type_json(),
+        user_agent = user_agent(userAgent)
+    )
+    content(x)
+}
