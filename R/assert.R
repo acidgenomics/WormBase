723ebd908972c9533ef8c427a3991f460d8e7820
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 17:23:14 -0500

    Add assert checks

---
+++ R/assert.R
@@ -0,0 +1,13 @@
+#' Assert Checks
+#'
+#' @rdname assert
+#' @name assert
+#' @family Assert Checks
+#' @keywords internal
+#'
+#' @importFrom assertive assert_all_are_matching_regex
+#' @importFrom assertive assert_is_a_string
+#' @importFrom assertive is_a_string
+#'
+#' @importFrom basejump assertIsAStringOrNULL
+NULL
