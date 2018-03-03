a67c79ec46826e29b7dfb8837cc413ffcda417f3
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 17:49:50 -0500

    Update assert checks

--- R/assert.R
+++ R/assert.R
@@ -7,7 +7,14 @@
 #'
 #' @importFrom assertive assert_all_are_matching_regex
 #' @importFrom assertive assert_is_a_string
+#' @importFrom assertive assert_is_character
 #' @importFrom assertive is_a_string
 #'
 #' @importFrom basejump assertIsAStringOrNULL
 NULL
+
+
+
+.assertAllAreGenes <- function(x) {
+    assert_all_are_matching_regex(x, "^WBGene[0-9]{8}$")
+}
