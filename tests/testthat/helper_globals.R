5f9e060aae119666a50aeef3f479d537f61b28b6
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-04 07:49:30 -0500

    Use `genes` helper global

---
+++ tests/testthat/helper_globals.R
@@ -0,0 +1,7 @@
+# Ensure these aren't sorted by ID to check internal code
+genes <- c(
+    "skn-1" = "WBGene00004804",
+    "sbp-1" = "WBGene00004735",
+    "daf-2" = "WBGene00000898",
+    "daf-16" = "WBGene00000912"
+)
