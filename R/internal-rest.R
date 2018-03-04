947b8799df97b7fb76c6607f684dadc80a8a2476
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-04 12:09:38 -0500

    Don't expose the main rest API caller to the user

--- R/internal-rest.R
+++ R/internal-rest.R
@@ -1,13 +1,5 @@
-#' RESTful API Query
-#'
-#' @family REST API Functions
-#' @keywords internal
-#'
 #' @importFrom httr content content_type_json GET user_agent
-#'
-#' @return [list].
-#' @export
-rest <- function(query) {
+.rest <- function(query) {
     x <- GET(
         url = paste("http://api.wormbase.org", "rest", query, sep = "/"),
         config = content_type_json(),
