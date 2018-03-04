3029e105301181899d6612775198f13630ed53c0
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 23:15:08 -0500

    Initial commit

---
+++ R/blastp.R
@@ -0,0 +1,31 @@
+#' Best BLASTP Hits
+#'
+#' @family Annotation File Functions
+#'
+#' @importFrom dplyr group_by mutate
+#' @importFrom magrittr set_colnames
+#' @importFrom readr read_csv
+#' @importFrom rlang !! sym
+#' @importFrom stringr str_sub
+#'
+#' @inheritParams annotationFile
+#'
+#' @return [tibble] grouped by `wormpep`.
+#' @export
+#'
+#' @examples
+#' blastp() %>% glimpse()
+blastp <- function(version = NULL, dir = ".") {
+    file <- annotationFile(
+        file = "best_blastp_hits",
+        version = version,
+        dir = dir
+    )
+    read_csv(file, col_names = FALSE) %>%
+        .[, c(1L, 4L, 5L)] %>%
+        set_colnames(c("wormpep", "peptide", "eValue")) %>%
+        .[grepl("^ENSEMBL:ENSP\\d{11}$", .[["peptide"]]), , drop = FALSE] %>%
+        .[order(.[["wormpep"]], .[["eValue"]]), ] %>%
+        mutate(peptide = str_sub(.data[["peptide"]], 9L)) %>%
+        group_by(!!sym("wormpep"))
+}
