3996df9e2567375221d5e3e59f0900e84401bf1d
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 21:18:15 -0500

    Initial commit of new flat file description parser

---
+++ R/description.R
@@ -0,0 +1,80 @@
+#' Gene Functional Descriptions
+#'
+#' @family Annotation File Functions
+#'
+#' @importFrom basejump camel
+#' @importFrom readr read_delim read_lines
+#'
+#' @inheritParams annotationFile
+#'
+#' @export
+#' @return [tibble]
+#'
+#' @examples
+#' description() %>% glimpse()
+description <- function(version = NULL, dir = ".") {
+    file <- annotationFile(
+        file = "functional_descriptions",
+        version = version,
+        dir = dir
+    )
+
+    # The first 3 lines contain comments
+    lines <- read_lines(file, skip = 3L)
+
+    # Genes are separated by a line containing `=`
+    lines <- gsub("^=$", "\\|\\|", lines)
+
+    # Add a tab delimiter before our keys of interest:
+    # - Concise description
+    # - Provisional description
+    # - Detailed description
+    # - Automated description
+    # - Gene class description
+    lines <- gsub(
+        pattern = paste0(
+            "(Concise|Provisional|Detailed|Automated|Gene class)",
+            " description\\:"
+        ),
+        replacement = "\t\\1 description:",
+        x = lines)
+
+    # Now collapse to a single line and split by the gene separator (`||`)
+    lines <- lines %>%
+        paste(collapse = " ") %>%
+        strsplit("\\|\\|") %>%
+        unlist()
+
+    # Clean up spaces and tabs
+    lines <- lines %>%
+        gsub("  ", " ", .) %>%
+        gsub("^ ", "", .) %>%
+        gsub(" $", "", .) %>%
+        gsub(" \t", "\t", .) %>%
+        gsub("\t ", "\t", .)
+
+    # Now split by the tab delimiters
+    lines <- strsplit(lines, "\t")
+
+    # Make this call parallel
+    dflist <- bplapply(lines, function(x) {
+        keyPattern <- "^([A-Za-z[:space:]]+)\\:"
+        names <- str_match(x, keyPattern)[, 2L]
+        names[1:3] <- c("gene", "name", "sequence")
+        names <- make.names(names)
+        # Now remove the keys
+        x <- gsub(paste0(keyPattern, " "), "", x)
+        # Sanitize `none available` to `NA`
+        x <- gsub("none available", NA, x)
+        names(x) <- names
+        tbl <- as_tibble(t(x))
+        # Ensure the user uses the values from `geneIDs()` instead
+        tbl[["name"]] <- NULL
+        tbl[["sequence"]] <- NULL
+        tbl
+    })
+
+    dflist %>%
+        bind_rows() %>%
+        camel()
+}
