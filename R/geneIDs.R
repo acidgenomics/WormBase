001756285f411211cd533804cc987971248dc736
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 19:14:22 -0500

    Initial commit

---
+++ R/geneIDs.R
@@ -0,0 +1,27 @@
+#' Gene Identifiers
+#'
+#' @family Annotation File Functions
+#'
+#' @importFrom readr read_csv
+#'
+#' @inheritParams annotationFile
+#'
+#' @return [tibble].
+#' @export
+#'
+#' @examples
+#' geneIDs() %>% glimpse()
+geneIDs <- function(version = NULL, dir = ".") {
+    file <- annotationFile(
+        file = "geneIDs",
+        version = version,
+        dir = dir
+    )
+    data <- read_csv(
+        file = file,
+        col_names = c("X1", "gene", "name", "sequence", "status"),
+        na = ""
+    )
+    data[["X1"]] <- NULL
+    data
+}
