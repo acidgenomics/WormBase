5d3923d69d7bcba653a46acde79a150d368b09ef
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 21:26:45 -0500

    Add `oligos()`

---
+++ R/oligos.R
@@ -0,0 +1,32 @@
+#' PCR Oligo Sequences
+#'
+#' @family Annotation File Functions
+#'
+#' @importFrom dplyr arrange group_by mutate select
+#' @importFrom readr read_tsv
+#' @importFrom rlang !! !!! sym syms
+#' @importFrom stringr str_extract
+#'
+#' @inheritParams annotationFile
+#'
+#' @return Grouped [tibble].
+#' @export
+#'
+#' @examples
+#' oligos() %>% glimpse()
+oligos <- function(version = NULL, dir = ".") {
+    file <- annotationFile(
+        file = "pcr_product2gene",
+        version = version,
+        dir = dir
+    )
+    df <-  suppressWarnings(read_tsv(
+        file,
+        col_names = c("oligo", "gene")
+    ))
+    df %>%
+        mutate(gene = str_extract(.data[["gene"]], "WBGene\\d{8}")) %>%
+        select(!!!syms(c("gene", "oligo"))) %>%
+        group_by(!!sym("gene")) %>%
+        arrange(!!sym("oligo"), .by_group = TRUE)
+}
