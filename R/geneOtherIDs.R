38785e8406a19fed05cb8eb87df9ac7b0d975f39
Michael Steinbaugh <mike@steinbaugh.com>
2018-03-03 19:13:48 -0500

    Initial commit

---
+++ R/geneOtherIDs.R
@@ -0,0 +1,36 @@
+#' Other Gene Identifiers
+#'
+#' @family Annotation File Functions
+#'
+#' @importFrom dplyr mutate
+#' @importFrom magrittr set_rownames
+#' @importFrom tibble as_tibble
+#'
+#' @inheritParams annotationFile
+#'
+#' @return [tibble].
+#' @export
+#'
+#' @examples
+#' geneOtherIDs() %>% glimpse()
+geneOtherIDs <- function(version = NULL, dir = ".") {
+    file <- annotationFile(
+        file = "geneOtherIDs",
+        version = version,
+        dir = dir
+    )
+    read_lines(file) %>%
+        # Remove status, already present in geneIDs file
+        gsub("\t(Dead|Live)", "", .) %>%
+        # Remove `CELE_*` identifiers
+        gsub("\t(CELE_[A-Z0-9\\.]+)", "", .) %>%
+        # Convert tabs to commas for identifiers
+        gsub("\t", "|", .) %>%
+        # Add tab back in to separate \code{gene} for row names
+        gsub("^(WBGene\\d+)(\\|)?", "\\1\t", .) %>%
+        strsplit("\t") %>%
+        do.call(rbind, .) %>%
+        as_tibble() %>%
+        set_colnames(c("gene", "otherIDs")) %>%
+        mutate(otherIDs = strsplit(.data[["otherIDs"]], "\\|"))
+}
