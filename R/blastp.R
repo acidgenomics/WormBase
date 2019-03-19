2426c2282b780f2a81d72629944e187628d15063
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-19 06:15:37 -0400

    Switch to importing via data.table::fread

--- R/blastp.R
+++ R/blastp.R
@@ -14,16 +14,14 @@ blastp <- function(version = NULL, dir = ".") {
         version = version,
         dir = dir
     )
-    read_csv(
-        file = as.character(file),
-        col_names = FALSE,
-        col_types = cols(),
-        progress = FALSE
-    ) %>%
+    import(unname(file), colnames = FALSE) %>%
         .[, c(1L, 4L, 5L)] %>%
         set_colnames(c("wormpep", "peptide", "eValue")) %>%
         .[grepl("^ENSEMBL:ENSP\\d{11}$", .[["peptide"]]), , drop = FALSE] %>%
         .[order(.[["wormpep"]], .[["eValue"]]), ] %>%
-        mutate(peptide = str_sub(!!sym("peptide"), 9L)) %>%
+        mutate(
+            peptide = str_sub(!!sym("peptide"), 9L),
+            eValue = as.numeric(!!sym("eValue"))
+        ) %>%
         group_by(!!sym("wormpep"))
 }
