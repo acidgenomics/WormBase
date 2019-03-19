e36ffd2dcf85e7909753889947503d0fb0ef472b
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-19 06:45:45 -0400

    Revert back to read_csv for geneIDs

--- R/imports.R
+++ R/imports.R
@@ -1,11 +1,11 @@
-#' @importFrom basejump camel import initializeDirectory removeNA sanitizeNA
+#' @importFrom basejump camel initializeDirectory removeNA sanitizeNA
 #'   toStringUnique transmit
 #' @importFrom goalie allAreMatchingRegex assert hasNoDuplicates isCharacter
 #'   isFlag isString
 #' @importFrom dplyr arrange bind_rows everything group_by mutate rename select
 #' @importFrom httr content content_type_json GET user_agent
 #' @importFrom magrittr set_colnames set_rownames
-#' @importFrom readr read_lines read_tsv
+#' @importFrom readr cols read_csv read_lines read_tsv
 #' @importFrom rlang !! !!! sym syms
 #' @importFrom stats aggregate formula
 #' @importFrom stringr str_extract str_extract_all str_match str_match_all
