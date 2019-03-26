e26f0c3b772324cdcfc3240798d7e162f17cdb62
Michael Steinbaugh <mike@steinbaugh.com>
2019-03-26 08:30:55 -0400

    Re-enable current production release

--- R/ftp-internal.R
+++ R/ftp-internal.R
@@ -38,19 +38,9 @@
         .isVersion(version),
         isString(dir)
     )
-
     # Prepare remote directory path for transmit call.
     if (is.null(version)) {
-        # version <- "current-production-release"  # nolint
-
-        # Temporary fix for buggy WS269 FTP files.
-        # In the process of contacting WormBase to fix this issue.
-        message(paste(
-            "Current production release (WS269) FTP files are malformed.",
-            "Returning WS268 release file instead.",
-            sep = "\n"
-        ))
-        version <- "WS268"
+        version <- "current-production-release"
     }
     releaseDir <- paste(
         "ftp://ftp.wormbase.org",
@@ -61,11 +51,8 @@
         sep = "/"
     )
     remoteDir <- paste(releaseDir, subdir, sep = "/")
-
     file <- transmit(remoteDir = remoteDir, localDir = dir, ...)
-
     # Check for single file match.
     assert(isString(file))
-
     file
 }
