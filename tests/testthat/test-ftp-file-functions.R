ab4ae8d8aa361ddba7d5607de26944c2785354fc
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-24 19:42:28 -0400

    Reorganize unit tests

--- tests/testthat/test-ftp-file-functions.R
+++ tests/testthat/test-ftp-file-functions.R
@@ -3,8 +3,8 @@ context("FTP File Functions")
 
 
 ## blastp ======================================================================
-test_that("blastp : Current", {
-    x <- blastp()
+test_that("blastp : Versioned", {
+    x <- blastp(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(
         lapply(x, class),
@@ -14,57 +14,57 @@ test_that("blastp : Current", {
             eValue = "numeric"
         )
     )
+    expect_identical(dim(x), c(14511L, 3L))
 })
 
-test_that("blastp : Versioned", {
-    x <- blastp(version = wbstable)
+test_that("blastp : Current", {
+    x <- blastp(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(dim(x), c(14511L, 3L))
 })
 
 
 
 ## description =================================================================
-test_that("description : Current", {
-    x <- description()
+test_that("description : Versioned", {
+    x <- description(version = wbstable)
     expect_is(x, "tbl_df")
     expect_true(all(vapply(
         X = x,
         FUN = is.character,
         FUN.VALUE = logical(1L)
     )))
+    expect_identical(nrow(x), ngene)
 })
 
-test_that("description : Versioned", {
-    x <- description(version = wbstable)
+test_that("description : Current", {
+    x <- description(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(nrow(x), ngene)
 })
 
 
 
 ## geneIDs =====================================================================
-test_that("geneIDs : Current", {
-    x <- geneIDs()
+test_that("geneIDs : Versioned", {
+    x <- geneIDs(version = wbstable)
     expect_is(x, "tbl_df")
     expect_true(all(vapply(
         X = x,
         FUN = is.character,
         FUN.VALUE = logical(1L)
     )))
+    expect_identical(nrow(x), ngene)
 })
 
-test_that("geneIDs : Versioned", {
-    x <- geneIDs(version = wbstable)
+test_that("geneIDs : Current", {
+    x <- geneIDs(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(nrow(x), ngene)
 })
 
 
 
 ## geneOtherIDs ================================================================
-test_that("geneOtherIDs : Current", {
-    x <- geneOtherIDs()
+test_that("geneOtherIDs : Versioned", {
+    x <- geneOtherIDs(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(
         lapply(x, class),
@@ -73,20 +73,19 @@ test_that("geneOtherIDs : Current", {
             geneOtherIDs = "list"
         )
     )
+    expect_identical(dim(x), c(ngene, 2L))
 })
 
-test_that("geneOtherIDs : Versioned", {
-    x <- geneOtherIDs(version = wbstable)
+test_that("geneOtherIDs : Current", {
+    x <- geneOtherIDs(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(dim(x), c(ngene, 2L))
 })
 
 
 
 ## oligos ======================================================================
-test_that("oligos : Current", {
-    x <- oligos()
-    expect_is(x, "tbl_df")
+test_that("oligos : Versioned", {
+    x <- oligos(version = wbstable)
     expect_identical(
         lapply(x, class),
         list(
@@ -94,19 +93,20 @@ test_that("oligos : Current", {
             oligo = "list"
         )
     )
-})
-
-test_that("oligos : Versioned", {
-    x <- oligos(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(dim(x), c(18833L, 2L))
+})
+
+test_that("oligos : Current", {
+    x <- oligos(version = NULL)
+    expect_is(x, "tbl_df")
 })
 
 
 
 ## orthologs ===================================================================
-test_that("orthologs : Current", {
-    x <- orthologs()
+test_that("orthologs : Versioned", {
+    x <- orthologs(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(
         lapply(x, class),
@@ -118,19 +118,19 @@ test_that("orthologs : Current", {
             danioRerio = "list"
         )
     )
+    expect_identical(dim(x), c(18966L, 5L))
 })
 
-test_that("orthologs : Versioned", {
-    x <- orthologs(version = wbstable)
+test_that("orthologs : Current", {
+    x <- orthologs(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(dim(x), c(18966L, 5L))
 })
 
 
 
 ## peptides ====================================================================
-test_that("peptides : Current", {
-    x <- peptides()
+test_that("peptides : Versioned", {
+    x <- peptides(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(
         lapply(x, class),
@@ -145,19 +145,19 @@ test_that("peptides : Current", {
             product = "character"
         )
     )
+    expect_identical(dim(x), c(28237L, 8L))
 })
 
-test_that("peptides : Versioned", {
-    x <- peptides(version = wbstable)
+test_that("peptides : Current", {
+    x <- peptides(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(dim(x), c(28237L, 8L))
 })
 
 
 
 ## rnaiPhenotypes ==============================================================
-test_that("rnaiPhenotypes : Current", {
-    x <- rnaiPhenotypes()
+test_that("rnaiPhenotypes : Versioned", {
+    x <- rnaiPhenotypes(version = wbstable)
     expect_is(x, "tbl_df")
     expect_identical(
         lapply(x, class),
@@ -166,10 +166,10 @@ test_that("rnaiPhenotypes : Current", {
             rnaiPhenotypes = "list"
         )
     )
+    expect_identical(dim(x), c(8010L, 2L))
 })
 
-test_that("rnaiPhenotypes : Versioned", {
-    x <- rnaiPhenotypes(version = wbstable)
+test_that("rnaiPhenotypes : Current", {
+    x <- rnaiPhenotypes(version = NULL)
     expect_is(x, "tbl_df")
-    expect_identical(dim(x), c(8010L, 2L))
 })
