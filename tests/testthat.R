983eff563b56779d26d7806a8b017e84d4f8c62c
Michael Steinbaugh <mike@steinbaugh.com>
2019-07-27 14:25:34 -0400

    Skip unit tests if WormBase server isn't accessible

--- tests/testthat.R
+++ tests/testthat.R
@@ -1,3 +1,10 @@
 library(testthat)
 library(wormbase)
-test_check("wormbase")
+## WormBase server must be online to run unit tests.
+check <- tryCatch(
+    expr = suppressMessages(currentRelease()),
+    error = function(e) e
+)
+if (!is(check, "error")) {
+    test_check("wormbase")
+}
--- tests/testthat/helper-connection.R
+++
@@ -1,9 +0,0 @@
-## WormBase server must be online to run unit tests.
-release <- tryCatch(
-    expr = suppressMessages(currentRelease()),
-    error = function(e) e
-)
-skip_if(
-    condition = is(release, "error"),
-    message = "Connection to WormBase FTP server failed."
-)
