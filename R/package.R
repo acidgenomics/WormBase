aa6a1e9e2cd8aed20cdd9136399fc28379b6224d
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-29 07:59:41 -0400

    Remove FIXME comments

--- R/package.R
+++ R/package.R
@@ -1,8 +1,3 @@
-## FIXME Switch to BiocParallel instead of pbapply
-## FIXME Return as `DataFrame` instead of `tbl_df` consistently.
-
-
-
 #' wormbase
 #'
 #' Fetch *Caenorhabditis elegans* genome annotations from WormBase.
