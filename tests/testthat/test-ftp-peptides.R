27b4b7dcb6873f7027f765d77b2a5e1ec27cdb48
Michael Steinbaugh <mike@steinbaugh.com>
2019-08-29 09:05:52 -0400

    Update unit test

--- tests/testthat/test-ftp-peptides.R
+++ tests/testthat/test-ftp-peptides.R
@@ -2,11 +2,11 @@ context("ftp : peptides")
 
 test_that("current", {
     x <- peptides(version = NULL)
-    expect_s4_class(x, "DataFrame")
+    expect_s4_class(x, "SplitDataFrameList")
 })
 
 test_that("stable", {
     x <- peptides(version = version)
-    expect_s4_class(x, "DataFrame")
-    expect_identical(nrow(x), 28420L)
+    expect_s4_class(x, "SplitDataFrameList")
+    expect_identical(length(x), 20191L)
 })
