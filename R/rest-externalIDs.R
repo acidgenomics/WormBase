52657d3804d2283ac69ff06f2f3587fb9be40e9a
Michael Steinbaugh <mike@steinbaugh.com>
2020-01-03 14:06:58 -0500

    Fix REST API examples

--- R/rest-externalIDs.R
+++ R/rest-externalIDs.R
@@ -1,12 +1,6 @@
-## FIXME The REST API is no longer working as expected.
-## Error in curl::curl_fetch_memory(url, handle = handle) :
-## Empty reply from server
-
-
-
 #' External identifiers
 #'
-#' @note Updated 2019-08-29.
+#' @note Updated 2020-01-03.
 #' @export
 #'
 #' @inheritParams params
