951a35c7436c68d6e2be743d0c83437f12e1d8c5
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-17 17:07:21 -0500

    Update NAMESPACE

--- R/package.R
+++ R/package.R
@@ -3,4 +3,15 @@
 #' Fetch *Caenorhabditis elegans* genome annotations from WormBase.
 #'
 #' @keywords internal
+#'
+#' @importFrom BiocParallel bplapply
+#' @importFrom basejump DataFrame DataFrameList aggregate alert camelCase
+#'   formula head import makeNames pasteURL rbindlist removeNA sanitizeNA split
+#'   t tail transmit unlist unsplit untar
+#' @importFrom goalie allAreMatchingRegex assert bapply hasLength
+#'   hasNoDuplicates isCharacter isFlag isString
+#' @importFrom httr content content_type_json GET user_agent
+#' @importFrom methods as
+#' @importFrom stringr str_extract str_extract_all str_match str_match_all
+#'   str_sub
 "_PACKAGE"
