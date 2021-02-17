ece9998bb7db6e882ac24bf3f26f4a54c7c2c70c
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-17 16:50:25 -0500

    Simplify the package NAMESPACE

--- R/imports.R
+++ R/imports.R
@@ -1,17 +1,21 @@
-#' @importFrom BiocGenerics unlist
-#' @importFrom BiocParallel bplapply
-#' @importFrom IRanges DataFrameList unsplit
-#' @importFrom S4Vectors DataFrame aggregate head split t tail
-#' @importFrom basejump camelCase import makeNames pasteURL removeNA sanitizeNA
-#'   toStringUnique transmit
-#' @importFrom cli cli_alert
-#' @importFrom data.table rbindlist
+#' @importFrom basejump DataFrame DataFrameList aggregate alert camelCase
+#'   formula head import makeNames pasteURL rbindlist removeNA sanitizeNA split
+#'   t tail transmit unlist unsplit
 #' @importFrom goalie allAreMatchingRegex assert bapply hasLength
 #'   hasNoDuplicates isCharacter isFlag isString
 #' @importFrom httr content content_type_json GET user_agent
 #' @importFrom methods as
-#' @importFrom stats formula
 #' @importFrom stringr str_extract str_extract_all str_match str_match_all
 #'   str_sub
-#' @importFrom utils untar
 NULL
+
+
+
+## FIXME MOVE THIS TO BASEJUMP.
+#' @importFrom stats formula
+
+## FIXME MOVE THIS TO BASEJUMP.
+#' @importFrom utils untar
+
+## FIXME MAKE THIS OPTIONAL.
+#' @importFrom BiocParallel bplapply
