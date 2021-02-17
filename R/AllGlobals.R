162dc134cfd45db704452375ffbfac45c803bbb2
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-17 18:15:58 -0500

    Improve documentation

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -1,20 +1,40 @@
-# Canonical reference sequence:
-#
-# PRJNA13758
-# https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA13758
-# Caenorhabditis elegans sequencing consortium project (2005)
-#
-# Other sequencing projects:
-#
-# PRJEB28388
-# https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJEB28388
-# Caenorhabditis elegans strain VC2010 genome sequencing project (2018)
-#
-# PRJNA275000
-# https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA275000
-# Caenorhabditis elegans Hawaiian strain CB4856 genome assembly (2015)
-
+#' BioProject to query from FTP server
+#'
+#' @note Updated 2021-02-17.
+#' @noRd
+#'
+#' @details
+#' Canonical reference sequence:
+#'
+#'     PRJNA13758
+#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA13758
+#'     Caenorhabditis elegans sequencing consortium project (2005)
+#'
+#' Other sequencing projects:
+#'
+#'     PRJEB28388
+#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJEB28388
+#'     Caenorhabditis elegans strain VC2010 genome sequencing project (2018)
+#'
+#'     PRJNA275000
+#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA275000
+#'     Caenorhabditis elegans Hawaiian strain CB4856 genome assembly (2015)
 .bioproject <- "PRJNA13758"
+
+#' Gene identifier pattern to use for grep matching
+#'
+#' @note Updated 2021-02-17.
+#' @noRd
 .genePattern <- "WBGene\\d{8}"
+
+#' User agent to use for REST API calls
+#'
+#' @note Updated 2021-02-17.
+#' @noRd
 .userAgent <- "https://r.acidgenomics.com/packages/wormbase/"
+
+#' Default version argument
+#'
+#' @note Updated 2021-02-17.
+#' @noRd
 .versionArg <- quote(getOption(x = "wormbase.version", default = NULL))
