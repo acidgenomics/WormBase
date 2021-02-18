e5084e8ba50a879f69bcd1a7978bb53386161388
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-18 11:19:51 -0500

    Rethink NAMESPACE

--- R/package.R
+++ R/package.R
@@ -6,13 +6,13 @@
 #'
 #' @importFrom basejump CharacterList DataFrame DataFrameList List aggregate
 #'   alert cacheURL camelCase do.call formula head import lapply makeNames
-#'   pasteURL rbindlist removeNA sanitizeNA split t tail transmit unlist
-#'   unlistToDataFrame unsplit untar
+#'   packageName pasteURL rbindlist removeNA sanitizeNA split t tail transmit
+#'   unlist unlistToDataFrame unsplit untar
 #' @importFrom goalie allAreMatchingRegex assert bapply hasLength
 #'   hasNoDuplicates isAFile isAURL isCharacter isFlag isMatchingRegex isString
 #'   isSubset
 #' @importFrom httr content content_type_json GET user_agent
-#' @importFrom methods as
+#' @importFrom methods as is
 #' @importFrom stringr str_extract str_extract_all str_match str_match_all
 #'   str_sub
 "_PACKAGE"
