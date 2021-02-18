154d6f7db0e9698bf3c7de5352bf25cb46ceeb02
Michael Steinbaugh <mike@steinbaugh.com>
2021-02-18 15:54:38 -0500

    Update unit tests

--- tests/testthat/test-ftp-peptides.R
+++ tests/testthat/test-ftp-peptides.R
@@ -8,5 +8,18 @@ test_that("current", {
 test_that("stable", {
     x <- peptides(release = release)
     expect_s4_class(x, "SplitDataFrameList")
-    expect_identical(length(x), 20191L)
+    expect_identical(length(x), 19997L)
+    expect_identical(
+        object = x[[1L]],
+        expected = DataFrame(
+            "geneId" = "WBGene00000001",
+            "wormpep" = "CE23248",
+            "status" = "Confirmed",
+            "uniprot" = "G5EDP9",
+            "insdc" = "CCD66201.1",
+            "sequence" = "Y110A7A.10",
+            "locus" = "aap-1",
+            "product" = NA_character_
+        )
+    )
 })
