0554d1896fbc14f173e54dc527412780ea3df851
Michael Steinbaugh <mike@steinbaugh.com>
2021-03-12 13:21:11 -0500

    NAMESPACE update

--- R/package.R
+++ R/package.R
@@ -6,13 +6,12 @@
 #'
 #' @importFrom basejump CharacterList DataFrame DataFrameList List aggregate
 #'   alert cacheURL camelCase do.call formula head import lapply makeNames
-#'   packageName pasteURL rbindlist removeNA sanitizeNA split t tail transmit
-#'   unlist unlistToDataFrame unsplit untar
+#'   packageName pasteURL rbindlist removeNA sanitizeNA split str_extract
+#'   str_extract_all str_match str_match_all str_sub t tail transmit unlist
+#'   unlistToDataFrame unsplit untar
 #' @importFrom goalie allAreMatchingRegex assert bapply hasLength
 #'   hasNoDuplicates isAFile isAURL isCharacter isFlag isMatchingRegex isString
 #'   isSubset
 #' @importFrom httr content content_type_json GET user_agent
 #' @importFrom methods as is
-#' @importFrom stringr str_extract str_extract_all str_match str_match_all
-#'   str_sub
 "_PACKAGE"
