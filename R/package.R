b5b275c5bf68f796e3f04c2a24ecd61aec104b80
Michael Steinbaugh <mike@steinbaugh.com>
2021-08-20 14:27:17 -0400

    Consolidate documentation

--- R/package.R
+++ R/package.R
@@ -15,3 +15,20 @@
 #' @importFrom httr content content_type_json GET user_agent
 #' @importFrom methods as is
 "_PACKAGE"
+
+
+
+#' Parameters
+#'
+#' @name params
+#' @keywords internal
+#'
+#' @param file `character(1)`.
+#'   Annotation file name.
+#' @param genes `character`.
+#'   Gene identifiers (e.g. WBGene00004804).
+#' @param release `character(1)` or `NULL`.
+#'   WormBase release. If `NULL` (recommended), defaults to current production
+#'   release release available on the WormBase website. Legacy releases can be
+#'   specified as a character string (e.g. "WS267").
+NULL
