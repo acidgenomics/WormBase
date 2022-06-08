f55c608361a5b78eb0bad71a3ecd9420ed2d133e
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-08 13:33:26 -0400

    Update unit test

--- tests/testthat/test-ftp-geneOtherIDs.R
+++ tests/testthat/test-ftp-geneOtherIDs.R
@@ -8,7 +8,7 @@ test_that("stable", {
     expect_s4_class(x, "CharacterList")
     expect_length(x, ngene)
     expect_identical(
-        object = x[[1L]],
+        object = sort(x[[1L]]),
         expected = c("aap-1", "Y110A7A.10")
     )
 })
