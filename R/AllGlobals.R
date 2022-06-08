ecaec8d1e27102607a125b69eb3fee5d23a66880
Michael Steinbaugh <mike@steinbaugh.com>
2022-06-08 10:27:01 -0400

    Update documentation

--- R/AllGlobals.R
+++ R/AllGlobals.R
@@ -6,19 +6,19 @@
 #' @details
 #' Canonical reference sequence:
 #'
-#'     PRJNA13758
-#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA13758
-#'     Caenorhabditis elegans sequencing consortium project (2005)
+#' PRJNA13758
+#' https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA13758
+#' Caenorhabditis elegans sequencing consortium project (2005)
 #'
 #' Other sequencing projects:
 #'
-#'     PRJEB28388
-#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJEB28388
-#'     Caenorhabditis elegans strain VC2010 genome sequencing project (2018)
+#' PRJEB28388
+#' https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJEB28388
+#' Caenorhabditis elegans strain VC2010 genome sequencing project (2018)
 #'
-#'     PRJNA275000
-#'     https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA275000
-#'     Caenorhabditis elegans Hawaiian strain CB4856 genome assembly (2015)
+#' PRJNA275000
+#' https://www.ncbi.nlm.nih.gov/bioproject/?term=PRJNA275000
+#' Caenorhabditis elegans Hawaiian strain CB4856 genome assembly (2015)
 .bioproject <- "PRJNA13758"
 
 
