168031dac7c71261a8a9bebe44b2fbe5e5f9afd4
Michael Steinbaugh <mike@steinbaugh.com>
2023-09-25 15:21:23 -0400

    Rework string extraction

--- R/ftp-peptides.R
+++ R/ftp-peptides.R
@@ -15,25 +15,16 @@
 peptides <- function(release = NULL) {
     file <- .assemblyFile(stem = "wormpep_package.tar.gz", release = release)
     tempdir <- tempdir2()
-    releaseNumber <- strMatch(
-        x = file,
-        pattern = "WS([[:digit:]]{3})",
-        fixed = FALSE
-    )[1L, 2L]
+    releaseNumber <- strMatch(file, pattern = "WS([[:digit:]]{3})")[1L, 2L]
     ## Extract the individual table.
     wormpepTable <- paste0("wormpep.table", releaseNumber)
     status <- untar(tarfile = file, files = wormpepTable, exdir = tempdir)
     assert(identical(status, 0L))
     x <- import(con = file.path(tempdir, wormpepTable), format = "lines")
     unlink2(tempdir)
-    ## FIXME Switch back to mclapply when working.
-    ## [1] ">2RSSE.1a wormpep=CE32785 gene=WBGene00007064 locus=rga-9 status=Confirmed uniprot=A4F337 insdc=CCD61138.1 product=\"Rho-GAP domain-containing protein\""
-    x <- lapply(
+    x <- mclapply(
         X = x,
         FUN = function(x) {
-            ## FIXME Need to skip on no pattern.
-            print(x)
-            ## FIXME Need to handle empty string.
             seqPattern <- "^>([A-Za-z0-9\\.]+)\\s"
             sequence <- strMatch(
                 x = x,
@@ -41,10 +32,14 @@ peptides <- function(release = NULL) {
                 fixed = FALSE
             )[1L, 2L]
             x <- sub(pattern = seqPattern, replacement = "", x = x)
-            x <- strsplit(x, split = " ")[[1L]]
-            x <- AcidBase::strSplit(x = x, split = "=")
-            out <- x[, 2L]
-            names(out) <- x[, 1L]
+            x <- strMatchAll(
+                x = x,
+                pattern = "\\b([a-z]+)=(\"[^\"]+\"|[^ ]+\\b)",
+                fixed = FALSE
+            )[[1L]]
+            x[, 3L] <-  gsub(pattern = "\"", replacement = "", x = x[, 3L])
+            out <- x[, 3L]
+            names(out) <- x[, 2L]
             out[["sequence"]] <- sequence
             out
         }
